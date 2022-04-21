8b4198c05d870dc5f49c255990331fa783582bce
robisonwebdev <david@robisonweb.dev>
2022-04-21 12:23:36 -0600

    Imported Error, Home, Movie, and Show components.

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { Routes, Route, Outlet } from 'react-router-dom';
+import Error from './error/Error';
+import Home from './home/Home';
+import Movie from './movie/Movie';
+import Show from './show/Show';
 import '../../styles/main/Main.css';
 
 const Main = () => {
