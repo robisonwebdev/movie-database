7428ec0aa692f62dc5f568f504bb9ac8d2a6d448
robisonwebdev <david@robisonweb.dev>
2022-04-28 03:07:43 -0600

    Imported useEffect. Setup breakpoint value to 600. Setup useEffect to add and remove event listener to window resize, and call setWidth.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../styles/header/Header.css';
 
 const Header = () => {
     const [width, setWidth] = useState(window.innerWidth);
+    const breakpoint = 600;
+
+    useEffect(() => {
+        const handleWindowResize = () => {
+            setWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => window.removeEventListener('resize', handleWindowResize);
+    }, []);
 
     return (
         <header>Header</header>
