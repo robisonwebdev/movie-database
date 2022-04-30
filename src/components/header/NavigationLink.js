8968aef26a4c6dcdf822d85ce961f9f1c336e956
robisonwebdev <david@robisonweb.dev>
2022-04-30 14:22:51 -0600

    Destructured linkTo and title prop.

--- src/components/header/NavigationLink.js
+++ src/components/header/NavigationLink.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../styles/header/NavigationLink.css';
 
-const NavigationLink = () => {
+const NavigationLink = ({ linkTo, title }) => {
     return (
         <Link className='navigation_link'></Link>
     );
