5bc08cbc9140cb69e60d21c9e620b4d7dffcfad9
robisonwebdev <david@robisonweb.dev>
2022-05-01 12:38:55 -0600

    Imported useParams Setup movieID to get useParams.

--- src/components/main/movie/Movie.js
+++ src/components/main/movie/Movie.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import '../../../styles/main/movie/Movie.css';
 
 const Movie = () => {
+    const { movieID } = useParams();
+
     return (
-        <section className='movie'>Movie</section>
+        <section className='movie'>
+            Movie
+        </section>
     );
 };
 
