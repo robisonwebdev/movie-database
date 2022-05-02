c85cf276238debd00ef89ea062eb6e19912b413b
robisonwebdev <david@robisonweb.dev>
2022-05-02 14:41:55 -0600

    Imported useParams. Setup showID to useParams.

--- src/components/main/show/Show.js
+++ src/components/main/show/Show.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import '../../../styles/main/show/Show.css';
 
 const Show = () => {
+    const { showID } = useParams();
+
     return (
         <section className='show'>Show</section>
     );
