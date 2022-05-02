19a9d46b89a5d5b74035568fafa35b34f835112b
robisonwebdev <david@robisonweb.dev>
2022-05-02 14:40:54 -0600

    Imported useParams. Setup personID to useParams.

--- src/components/main/person/Person.js
+++ src/components/main/person/Person.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import '../../../styles/main/person/Person.css';
 
 const Person = () => {
+    const { personID } = useParams();
+
     return (
         <section className='person'>Person</section>
     );
