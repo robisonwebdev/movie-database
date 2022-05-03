050ad03a4ca7fe491585eac939d9ac492e1e94d0
robisonwebdev <david@robisonweb.dev>
2022-05-03 12:04:01 -0600

    Imported NavigationLink. Added ul and NavigationLink's to return div.

--- src/components/header/MobileLinks.js
+++ src/components/header/MobileLinks.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import NavigationLink from './NavigationLink';
 import '../../styles/header/MobileLinks.css';
 
 const MobileLinks = () => {
     return (
-        <div className='mobile_links'>MobileLinks</div>
+        <div className='mobile_links'>
+            <ul>
+                <NavigationLink linkTo='movies' title='Movies' />
+                <NavigationLink linkTo='tvShows' title='TV Shows' />
+                <NavigationLink linkTo='people' title='People' />
+            </ul>
+        </div>
     );
 };
 
