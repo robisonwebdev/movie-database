77282427781710216d3a56801097e89741b22dcd
robisonwebdev <david@robisonweb.dev>
2022-05-03 11:44:45 -0600

    Imported NavigationLogo. Added NavigationLogo to .mobile_header

--- src/components/header/MobileNavigation.js
+++ src/components/header/MobileNavigation.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import NavigationLink from './NavigationLink';
+import NavigationLogo from './NavigationLogo';
 import '../../styles/header/MobileNavigation.css';
 
 const MobileNavigation = () => {
     return (
         <nav className='mobile_navigation'>
-            <div className='mobile_header'></div>
+            <div className='mobile_header'>
+                <NavigationLogo />
+            </div>
             <div className='mobile_links'></div>
         </nav>
     );
