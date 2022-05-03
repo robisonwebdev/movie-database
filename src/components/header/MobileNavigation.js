7acca9a953d2496db832a41f8d7c70d47547a4fd
robisonwebdev <david@robisonweb.dev>
2022-05-03 11:50:15 -0600

    Imported CloseIcon. Set menuActive state and set to an initial value of false.

--- src/components/header/MobileNavigation.js
+++ src/components/header/MobileNavigation.js
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
+import CloseIcon from '@mui/icons-material/Close';
 import MenuIcon from '@mui/icons-material/Menu';
 import NavigationLink from './NavigationLink';
 import NavigationLogo from './NavigationLogo';
 import '../../styles/header/MobileNavigation.css';
 
 const MobileNavigation = () => {
+    const {menuActive, setMenuActive} = useState(false);
+
     return (
         <nav className='mobile_navigation'>
             <div className='mobile_header'>
