80cc7ae9445296de4a562c745d08b3c0d5e33987
robisonwebdev <david@robisonweb.dev>
2022-05-07 08:35:41 -0600

    Imported PersonalInformation component. Added img comment and PersonalInformation component inside of .person_left_section

--- src/components/main/person/Person.js
+++ src/components/main/person/Person.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import PersonalInformation from './PersonalInformation';
 import '../../../styles/main/person/Person.css';
 
 const Person = () => {
@@ -7,7 +8,10 @@ const Person = () => {
 
     return (
         <section className='person'>
-            <section className='person_left_section'></section>
+            <section className='person_left_section'>
+                {/* Img */}
+                <PersonalInformation />
+            </section>
             <section className='person_right_section'></section>
         </section>
     );
