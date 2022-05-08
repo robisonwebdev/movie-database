7828f7d10cddc5c3bfa9c3da8e31a6e148c5875f
robisonwebdev <david@robisonweb.dev>
2022-05-08 15:31:45 -0600

    Added temp img src.

--- src/components/main/person/PersonImage.js
+++ src/components/main/person/PersonImage.js
@@ -4,7 +4,7 @@ import '../../../styles/main/person/PersonImage.css';
 const PersonImage = () => {
     return (
         <>
-            <img className='person_image'/>
+            <img className='person_image' src='https://images.unsplash.com/photo-1555952517-2e8e729e0b44?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fHBlcnNvbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'/>
         </>
     );
 };
