8fd5eeb82632af8e189c76c966920fff16db2bd8
robisonwebdev <david@robisonweb.dev>
2022-05-09 08:57:22 -0600

    Destructured onClick prop. Passed onClick prop to onClick event on Link component.

--- src/components/header/NavigationLink.js
+++ src/components/header/NavigationLink.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../styles/header/NavigationLink.css';
 
-const NavigationLink = ({ linkTo, title }) => {
+const NavigationLink = ({ linkTo, onClick, title }) => {
     return (
-        <Link className='navigation_link' to={linkTo}>
+        <Link className='navigation_link' onClick={() => onClick(false)} to={linkTo}>
             <li>{title}</li>
         </Link>
     );
