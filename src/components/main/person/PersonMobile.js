ae28e01b8957e084808d678cd63600229671dd3d
robisonwebdev <david@robisonweb.dev>
2022-05-09 09:42:21 -0600

    Imported PersonImage. Added PersonImage to return section.

--- src/components/main/person/PersonMobile.js
+++ src/components/main/person/PersonMobile.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import PersonImage from './PersonImage';
 import '../../../styles/main/person/PersonMobile.css';
 
 const PersonMobile = () => {
     return (
-        <section className='person_mobile'>PersonMobile</section>
+        <section className='person_mobile'>
+            <PersonImage />
+        </section>
     );
 };
 
