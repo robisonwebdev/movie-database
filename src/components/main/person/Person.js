6f3507568e387fed20d61a1571124223e086ffe7
robisonwebdev <david@robisonweb.dev>
2022-05-10 10:33:13 -0600

    Imported useCallback and axios. Setup state for loading and personInformation. Setup initial fetchData function, using useCallback. Currently updates personInformation with biography, imagePath, and name. Called fetchData using useEffect. Passed personInformation down to PersonMobile and PersonDesktop.

--- src/components/main/person/Person.js
+++ src/components/main/person/Person.js
@@ -1,15 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import PersonDesktop from './PersonDesktop';
 import PersonMobile from './PersonMobile';
 import '../../../styles/main/person/Person.css';
 
 const Person = () => {
+    const [loading, setLoading] = useState(true);
+    const [personInformation, setPersonInformation] = useState({})
     const [width, setWidth] = useState(window.innerWidth);
     const { personID } = useParams();
     const breakpoint = 600;
 
+    const fetchData = useCallback(() => {
+        const person_API = `https://api.themoviedb.org/3/person/${personID}?api_key=9289aca3a6413b200619b263ac82e4c0&language=en-US`;
+
+        setLoading(true);
+
+        axios
+        .get(person_API)
+        .then(res => {
+            setPersonInformation(info => ({
+                ...info,
+                biography: res.data.biography,
+                imagePath: res.data.profile_path,
+                name: res.data.name
+            }));
+            setLoading(false);
+        })
+        .catch(err => console.log(err))
+
+    }, [personID]);
+
     useEffect(() => {
+        fetchData();
+
         const handleWindowResize = () => {
             setWidth(window.innerWidth);
         };
@@ -17,11 +42,12 @@ const Person = () => {
         window.addEventListener('resize', handleWindowResize);
 
         return () => window.removeEventListener('resize', handleWindowResize);
-    }, []);
+    }, [fetchData]);
 
     return (
         <section className='person'>
-            {width < breakpoint ? <PersonMobile /> : <PersonDesktop />}
+            {loading ? null : console.log(personInformation)}
+            {width < breakpoint ? <PersonMobile personInformation={personInformation} /> : <PersonDesktop personInformation={personInformation} />}
         </section>
     );
 };
