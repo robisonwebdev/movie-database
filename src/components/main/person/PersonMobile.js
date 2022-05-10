dab0783c652a14c9b914929d56a0c1ef6571e726
robisonwebdev <david@robisonweb.dev>
2022-05-10 10:12:53 -0600

    Destructured personInformation and destrucutred biography and name from personInformation. Passed name to h1. Passed biography to Biography component.

--- src/components/main/person/PersonMobile.js
+++ src/components/main/person/PersonMobile.js
@@ -3,12 +3,14 @@ import Biography from './Biography';
 import PersonImage from './PersonImage';
 import '../../../styles/main/person/PersonMobile.css';
 
-const PersonMobile = () => {
+const PersonMobile = ({ personInformation }) => {
+    const { biography, name } = personInformation;
+
     return (
         <section className='person_mobile'>
             <PersonImage />
-            <h1>PersonName</h1>
-            <Biography />
+            <h1>{name}</h1>
+            <Biography biography={biography} />
         </section>
     );
 };
