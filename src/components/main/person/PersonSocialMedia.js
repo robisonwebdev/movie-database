cc5e82ff3bbc4fb746f09e7e4506734d0a945fd7
robisonwebdev <david@robisonweb.dev>
2022-05-12 10:39:40 -0600

    Imported FacebookIcon, InstagramIcon, and TwitterIcon. Setup PersonSocialMedia to map over an array containing an individuals social media links. If no links return null, else return an li with icon with address to social media site.

--- src/components/main/person/PersonSocialMedia.js
+++ src/components/main/person/PersonSocialMedia.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
-const PersonSocialMedia = () => {
+const PersonSocialMedia = ({ socialMedia }) => {
+    const { facebook, instagram, twitter } = socialMedia;
+    const socialMediaArray = [
+        {
+            address: `https://www.facebook.com/${facebook}`,
+            id: `${facebook}_1`,
+            icon: <FacebookIcon />,
+            platform: facebook
+        },
+        {
+            address: `https://www.instagram.com/${instagram}`,
+            id: `${instagram}_2`,
+            icon: <InstagramIcon />,
+            platform: instagram
+        },
+        {
+            address: `https://twitter.com/${twitter}`,
+            id: `${twitter}_3`,
+            icon: <TwitterIcon />,
+            platform: twitter
+        }
+    ];
+
+    const mapSocialMedia = socialMediaArray?.map(media => {
+        if (media.platform === '' || media.platform === null) return null;
+
+        return (
+            <li key={media.id}>
+                <a href={media.address} rel='noreferrer' target='_blank'>
+                    {media.icon}
+                </a>
+            </li>
+        );
+    })
+
     return (
-        <div>PersonSocialMedia</div>
+        <ul className='person_social_media'>
+            {mapSocialMedia}
+        </ul>
     );
 };
 
