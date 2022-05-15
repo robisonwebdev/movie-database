9fe89fbb37e2aac926efc604971bd6cefba7764b
robisonwebdev <david@robisonweb.dev>
2022-05-15 08:48:02 -0600

    imported PersonalInformation and PersonImage components. Added imported components to .left_container

--- src/components/main/person/PersonDesktop.js
+++ src/components/main/person/PersonDesktop.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import PersonalInformation from './PersonalInformation';
+import PersonImage from './PersonImage';
 import '../../../styles/main/person/PersonDesktop.css';
 
 const PersonDesktop = () => {
     return (
         <section className='person_desktop'>
-            <section className='left_container'></section>
+            <section className='left_container'>
+                <PersonImage />
+                <PersonalInformation />
+            </section>
             <section className='right_container'></section>
         </section>
     );
