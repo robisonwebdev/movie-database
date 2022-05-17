8ee37fe66b15cfcb3641dd3ff2d90d7a428de818
David Robison <drbr56@gmail.com>
2022-05-16 19:09:47 -0600

    Imported useEffect and useState. Setup width state, and gave an initial value of window.innerWidth Setup useEffect to adjusted width state.

--- src/components/main/person/PersonTitle.js
+++ src/components/main/person/PersonTitle.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PersonSocialMedia from './PersonSocialMedia';
 import '../../../styles/main/person/PersonTitle.css';
 
 const PersonTitle = ({ personInformation }) => {
+    const [width, setWidth] = useState(window.innerWidth);
     const { name } = personInformation;
+    const breakpoint = 600;
+
+    useEffect(() => {
+        const handleWindowResize = () => {
+            setWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => window.removeEventListener('resize', handleWindowResize);
+    }, []);
 
     return (
         <div className='person_title'>
