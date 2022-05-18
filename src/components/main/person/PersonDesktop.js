a37257235dbe89412813233c9e390dd77e4617f4
David Robison <drbr56@gmail.com>
2022-05-18 15:29:10 -0600

    Imported PersonSocialMedia component. Added PersonSocialMedia component to .left_container and passed it personInformation prop.

--- src/components/main/person/PersonDesktop.js
+++ src/components/main/person/PersonDesktop.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Biography from './Biography';
 import PersonalInformation from './PersonalInformation';
 import PersonImage from './PersonImage';
+import PersonSocialMedia from './PersonSocialMedia';
 import PersonTitle from './PersonTitle';
 import '../../../styles/main/person/PersonDesktop.css';
 
@@ -12,6 +13,7 @@ const PersonDesktop = ({ personInformation }) => {
         <section className='person_desktop'>
             <section className='left_container'>
                 <PersonImage name={name} path={imagePath} />
+                <PersonSocialMedia socialMedia={personInformation} />
                 <PersonalInformation personalInformation={personInformation} />
             </section>
             <section className='right_container'>
