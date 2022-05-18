d9eed83a144d6cd2fa247d931976483657a85a74
David Robison <drbr56@gmail.com>
2022-05-18 15:51:00 -0600

    Set Format component to return div with h3 and p children. h3 takes title prop and p takes info prop. Replaced return in getDeathday with Format component. Set getBirthday function to return birthday using Format if not null.

--- src/components/main/person/PersonalInformation.js
+++ src/components/main/person/PersonalInformation.js
@@ -4,23 +4,34 @@ import '../../../styles/main/person/PersonalInformation.css';
 const PersonalInformation = ({ personalInformation }) => {
     const { birthday, birthPlace, deathday, gender, knownFor } = personalInformation;
 
+    const getBirthday = () => {
+        if (birthday === null) return null;
+
+        return <Format info={birthday} title='Birthday' />
+    };
+
     const getDeathday = () => {
         if (deathday === null) return null;
 
-        return (
-            <>
-                <h3>Death</h3>
-                <p>{deathday}</p>
-            </>
-        );
+        return <Format info={deathday} title='Death' />
     };
 
     return (
         <section className='personal_information'>
             <h2>Personal Information</h2>
+            {getBirthday()}
             {getDeathday()}
         </section>
     );
 };
+
+const Format = ({ info, title }) => {
+    return (
+        <div>
+            <h3>{title}</h3>
+            <p>{info}</p>
+        </div>
+    );
+}
 
 export default PersonalInformation;
