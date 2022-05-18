6bed4f08ffdcb674fbb7ab469c1cbfb3f69121b3
David Robison <drbr56@gmail.com>
2022-05-18 15:39:30 -0600

    Setup getDeathday function to return deathday if not null. Called getDeathday in .personal_information

--- src/components/main/person/PersonalInformation.js
+++ src/components/main/person/PersonalInformation.js
@@ -4,9 +4,21 @@ import '../../../styles/main/person/PersonalInformation.css';
 const PersonalInformation = ({ personalInformation }) => {
     const { birthday, birthPlace, deathday, gender, knownFor } = personalInformation;
 
+    const getDeathday = () => {
+        if (deathday === null) return null;
+
+        return (
+            <>
+                <h3>Death</h3>
+                <p>{deathday}</p>
+            </>
+        );
+    };
+
     return (
         <section className='personal_information'>
             <h2>Personal Information</h2>
+            {getDeathday()}
         </section>
     );
 };
