4a9a4f6c48e7fef644fd7922ba5462b446d55026
David Robison <drbr56@gmail.com>
2022-05-19 15:04:23 -0600

    Removed useState and useEffect imports. Removed width state, breakpoint variable, and useEffect. Replaced ternary arguments with format.

--- src/components/main/person/PersonTitle.js
+++ src/components/main/person/PersonTitle.js
@@ -1,26 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import PersonSocialMedia from './PersonSocialMedia';
 import '../../../styles/main/person/PersonTitle.css';
 
 const PersonTitle = ({ format, personInformation }) => {
-    const [width, setWidth] = useState(window.innerWidth);
     const { name } = personInformation;
-    const breakpoint = 865;
-
-    useEffect(() => {
-        const handleWindowResize = () => {
-            setWidth(window.innerWidth);
-        };
-
-        window.addEventListener('resize', handleWindowResize);
-
-        return () => window.removeEventListener('resize', handleWindowResize);
-    }, []);
 
     return (
-        <div className={width < breakpoint ? 'person_title_mobile' : 'person_title_desktop'}>
+        <div className={format === 'mobile' ? 'person_title_mobile' : 'person_title_desktop'}>
             <h1>{name}</h1>
-            {width < breakpoint ? <PersonSocialMedia socialMedia={personInformation} /> : null}
+            {format === 'mobile' ? <PersonSocialMedia socialMedia={personInformation} /> : null}
         </div>
     );
 };
