92eed5381628676a48301e6898c4132f6b4f2d7f
David Robison <drbr56@gmail.com>
2022-05-19 15:01:31 -0600

    Added format to destructured props.

--- src/components/main/person/PersonImage.js
+++ src/components/main/person/PersonImage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../../styles/main/person/PersonImage.css';
 
-const PersonImage = ({ name, path }) => {
+const PersonImage = ({ format, name, path }) => {
     const imageSrc = `https://www.themoviedb.org/t/p/w600_and_h900_bestv2${path}`;
 
     return (
