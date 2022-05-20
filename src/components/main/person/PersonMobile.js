4ea340fb07472f9220b8f2a048537d403ae0ed06
David Robison <drbr56@gmail.com>
2022-05-20 14:41:14 -0600

    Imported PersonKnownFor component. Added PersonKnownFor component to .person_mobile

--- src/components/main/person/PersonMobile.js
+++ src/components/main/person/PersonMobile.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Biography from './Biography';
 import PersonImage from './PersonImage';
 import PersonalInformation from './PersonalInformation';
+import PersonKnownFor from './PersonKnownFor';
 import PersonTitle from './PersonTitle';
 import '../../../styles/main/person/PersonMobile.css';
 
@@ -14,6 +15,7 @@ const PersonMobile = ({ personInformation }) => {
             <PersonTitle format='mobile' personInformation={personInformation} />
             <Biography biography={biography} format='mobile' />
             <PersonalInformation format='mobile' personalInformation={personInformation} />
+            <PersonKnownFor />
         </section>
     );
 };
