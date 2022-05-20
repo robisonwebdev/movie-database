d2a4ef7a9390a1cb1e1a8ad1b84082d9309cdf33
David Robison <drbr56@gmail.com>
2022-05-20 15:09:26 -0600

    Destrucutred credits and format from props.

--- src/components/main/person/PersonKnownFor.js
+++ src/components/main/person/PersonKnownFor.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../../styles/main/person/PersonKnownFor.css';
 
-const PersonKnownFor = () => {
+const PersonKnownFor = ({ credits, format }) => {
     return (
         <section>PersonKnownFor</section>
     );
