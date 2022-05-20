ee251981150345b74dd2fb72f0b9b1c1eeeeed0f
David Robison <drbr56@gmail.com>
2022-05-20 15:13:12 -0600

    Setup className for return section. ClassName is determined based on format.

--- src/components/main/person/PersonKnownFor.js
+++ src/components/main/person/PersonKnownFor.js
@@ -3,7 +3,9 @@ import '../../../styles/main/person/PersonKnownFor.css';
 
 const PersonKnownFor = ({ credits, format }) => {
     return (
-        <section>PersonKnownFor</section>
+        <section className={format === 'mobile' ? 'person_known_for_mobile' : 'person_known_for_desktop'}>
+            
+        </section>
     );
 };
 
