7a1f49a62b210e7476cf0828f9d389913432f7d5
David Robison <drbr56@gmail.com>
2022-05-21 17:06:01 -0600

    Setup topCredits variable that sorts credits prop from highest vote count to lowest vote count.

--- src/components/main/person/PersonKnownFor.js
+++ src/components/main/person/PersonKnownFor.js
@@ -2,6 +2,8 @@ import React from 'react';
 import '../../../styles/main/person/PersonKnownFor.css';
 
 const PersonKnownFor = ({ credits, format }) => {
+    const topCredits = credits.sort((a, b) => b.vote_count - a.vote_count);
+
     return (
         <section className={format === 'mobile' ? 'person_known_for_mobile' : 'person_known_for_desktop'}>
             
