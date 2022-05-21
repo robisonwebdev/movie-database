289a6e775d6beba555d721e3d5b4c2e2a67933d4
David Robison <drbr56@gmail.com>
2022-05-21 17:07:33 -0600

    Added slice onto topCredits to only give the top 10 highest vote_count objects.

--- src/components/main/person/PersonKnownFor.js
+++ src/components/main/person/PersonKnownFor.js
@@ -2,11 +2,11 @@ import React from 'react';
 import '../../../styles/main/person/PersonKnownFor.css';
 
 const PersonKnownFor = ({ credits, format }) => {
-    const topCredits = credits.sort((a, b) => b.vote_count - a.vote_count);
+    const topCredits = credits.sort((a, b) => b.vote_count - a.vote_count).slice(0, 10);
 
     return (
         <section className={format === 'mobile' ? 'person_known_for_mobile' : 'person_known_for_desktop'}>
-            
+            {console.log(topCredits)}
         </section>
     );
 };
