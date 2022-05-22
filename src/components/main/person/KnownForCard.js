0cc02de2cef94b00e220446dd862cb736e85aa51
David Robison <drbr56@gmail.com>
2022-05-22 15:26:47 -0600

    Imported MovieIcon. Destructured deatils and format. Destrucuted name, poster_path, and title from details. Setup getImageSrc function to check for poster path, returning MovieIcon is not found or path to poster. Setup ternary on section className to changed className if in mobile or desktop view/ Add img and p tag to return section. Passed getImageSrc to img and set alt to name or title. Passed name or title to p tag.

--- src/components/main/person/KnownForCard.js
+++ src/components/main/person/KnownForCard.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import MovieIcon from '@mui/icons-material/Movie';
 import '../../../styles/main/person/KnownForCard.css';
 
-const KnownForCard = () => {
+const KnownForCard = ({ details, format }) => {
+    const { name, poster_path, title} = details;
+    
+    const getImageSrc = () => {
+        if (poster_path === null || poster_path === '') return <MovieIcon />
+
+        return `https://www.themoviedb.org/t/p/w150_and_h225_bestv2${poster_path}`;
+    };
+
     return (
-        <section className='known_for_card'>KnownForCard</section>
+        <section className={format === 'mobile' ? 'known_for_card_mobile' : 'known_for_card_desktop'}>
+            <img src={getImageSrc()} alt={name || title} />
+            <p>{name || title }</p>
+        </section>
     );
 };
 
