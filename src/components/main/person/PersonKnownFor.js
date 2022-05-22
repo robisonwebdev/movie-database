4230a6bfc021c6a8f1242a0f6d2fe6bb97bb7118
David Robison <drbr56@gmail.com>
2022-05-22 15:01:49 -0600

    Setup mapTopCredits variable to map overs topCredit array, returning KnownForCard component for each item. Called mapTopCredits in return section.

--- src/components/main/person/PersonKnownFor.js
+++ src/components/main/person/PersonKnownFor.js
@@ -5,9 +5,13 @@ import '../../../styles/main/person/PersonKnownFor.css';
 const PersonKnownFor = ({ credits, format }) => {
     const topCredits = credits.sort((a, b) => b.vote_count - a.vote_count).slice(0, 10);
 
+    const mapTopCredits = topCredits.map(credit => {
+        return <KnownForCard key={credit.id} details={credit} />
+    });
+
     return (
         <section className={format === 'mobile' ? 'person_known_for_mobile' : 'person_known_for_desktop'}>
-            {console.log(topCredits)}
+            {mapTopCredits}
         </section>
     );
 };
