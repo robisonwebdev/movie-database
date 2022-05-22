81e7a4daf5239ff29e937581e37a1c6d09b3b3c8
David Robison <drbr56@gmail.com>
2022-05-22 15:27:49 -0600

    Passed format prop down to KnownForCard. Added h3 to return section.

--- src/components/main/person/PersonKnownFor.js
+++ src/components/main/person/PersonKnownFor.js
@@ -6,12 +6,14 @@ const PersonKnownFor = ({ credits, format }) => {
     const topCredits = credits.sort((a, b) => b.vote_count - a.vote_count).slice(0, 10);
 
     const mapTopCredits = topCredits.map(credit => {
-        return <KnownForCard key={credit.id} details={credit} />
+        return <KnownForCard key={credit.id} details={credit} format={format} />
     });
 
     return (
         <section className={format === 'mobile' ? 'person_known_for_mobile' : 'person_known_for_desktop'}>
+            <h3>Known For</h3>
             {mapTopCredits}
+            {console.log(topCredits)}
         </section>
     );
 };
