1e545dd12032eb0ed1fa290b381f500dacdea1b5
David Robison <drbr56@gmail.com>
2022-05-23 09:51:32 -0600

    Replaced getImageSrc function with getPoster function. getPoster either returns MovieIcon if not poster_path src, or an img set to poster_path. Removed img tag from return section and replaced with getPoster.

--- src/components/main/person/KnownForCard.js
+++ src/components/main/person/KnownForCard.js
@@ -4,16 +4,16 @@ import '../../../styles/main/person/KnownForCard.css';
 
 const KnownForCard = ({ details, format }) => {
     const { name, poster_path, title} = details;
-    
-    const getImageSrc = () => {
-        if (poster_path === null || poster_path === '') return <MovieIcon />
 
-        return `https://www.themoviedb.org/t/p/w150_and_h225_bestv2${poster_path}`;
+    const getPoster = () => {
+        if (poster_path === null || poster_path === '') return <MovieIcon />;
+
+        return <img src={`https://www.themoviedb.org/t/p/w150_and_h225_bestv2${poster_path}`} alt={name || title} />
     };
 
     return (
         <section className={format === 'mobile' ? 'known_for_card_mobile' : 'known_for_card_desktop'}>
-            <img src={getImageSrc()} alt={name || title} />
+            {getPoster()}
             <p>{name || title }</p>
         </section>
     );
