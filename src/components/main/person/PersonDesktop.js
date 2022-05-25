f1c05718d1ffc9904e67af8f9c2fb8aed4d814e9
David Robison <drbr56@gmail.com>
2022-05-25 15:36:49 -0600

    Passed combinedCredits down to PersonKnownFor, and set components format to desktop

--- src/components/main/person/PersonDesktop.js
+++ src/components/main/person/PersonDesktop.js
@@ -20,7 +20,7 @@ const PersonDesktop = ({ personInformation }) => {
             <section className='right_container'>
                 <PersonTitle format='desktop' personInformation={personInformation} />
                 <Biography biography={biography} format='desktop' />
-                <PersonKnownFor />
+                <PersonKnownFor credits={combinedCredits} format='desktop' />
             </section>
         </section>
     );
