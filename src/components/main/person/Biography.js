358d9eff740736761cea3797150b5171015059ff
David Robison <drbr56@gmail.com>
2022-05-26 09:27:41 -0600

    Created new function called getBiography that return 'Biography unavailable' or biography prop. Replaced biography prop in p tag with getBiography.

--- src/components/main/person/Biography.js
+++ src/components/main/person/Biography.js
@@ -2,11 +2,16 @@ import React from 'react';
 import '../../../styles/main/person/Biography.css';
 
 const Biography = ({ biography, format }) => {
+    const getBiography = () => {
+        if (biography === '' || biography === null) return 'Biography unavailable.';
+
+        return biography;
+    };
 
     return (
         <section className={format === 'mobile' ? 'biography_mobile' : 'biography_desktop'}>
             <h2>Biography</h2>
-            <p>{biography}</p>
+            <p>{getBiography()}</p>
         </section>
     );
 };
