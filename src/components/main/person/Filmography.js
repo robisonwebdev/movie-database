bdda36ac429f0e7350da08c92e04673e63534b75
David Robison <drbr56@gmail.com>
2022-05-29 10:13:07 -0600

    Setup state for loading. Setup getJobList callback, that uses axios to get a list of jobs from TMDB. Sets list to setJobList. Setup useEffect to call getJobList.

--- src/components/main/person/Filmography.js
+++ src/components/main/person/Filmography.js
@@ -4,10 +4,34 @@ import '../../../styles/main/person/Filmography.css';
 
 const Filmography = ({ combinedCredits }) => {
     const [jobList, setJobList] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const getJobList = useCallback(() => {
+        const jobList_API = `https://api.themoviedb.org/3/configuration/jobs?api_key=9289aca3a6413b200619b263ac82e4c0`;
+
+        setLoading(true);
+
+        axios
+            .get(jobList_API)
+            .then(res => {
+                setJobList(res.data);
+                setLoading(false);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, []);
+
+    useEffect(() => {
+        getJobList();
+    }, [getJobList]);
 
     console.log(combinedCredits)
     return (
-        <section className='filmography'>Filmography</section>
+        <section className='filmography'>
+            Filmography
+            {loading ? null : console.log(jobList)}
+        </section>
     );
 };
 
