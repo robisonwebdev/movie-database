d7b4e3e1c934fff955098a78e6b8ca2ef795b4c5
David Robison <drbr56@gmail.com>
2022-05-31 16:11:28 -0600

    Imported ArrowDropDownIcon and ArrowDropUpIcon

--- src/components/main/person/FilmographyCategory.js
+++ src/components/main/person/FilmographyCategory.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import FilmographyCard from './FilmographyCard';
 import '../../../styles/main/person/FilmographyCategory.css';
 
 const FilmographyCategory = () => {
     return (
-        <section className='filmography_category'>FilmographyCategory</section>
+        <section className='filmography_category'>
+
+        </section>
     );
 };
 
