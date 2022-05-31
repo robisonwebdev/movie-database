a00a383c8812e077fbdde187afe6f8871c2d39c6
David Robison <drbr56@gmail.com>
2022-05-31 16:23:14 -0600

    Imported useState. Setup show state, with an initial value of false. Added two p tag's to .category_right. Inside both p tags, setup ternary operator set to show state. First ternary operator will either display text 'show' or 'hide' Second ternary operator will either show ArrowDropUpIcon or ArrowDropDownIcon.

--- src/components/main/person/FilmographyCategory.js
+++ src/components/main/person/FilmographyCategory.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import FilmographyCard from './FilmographyCard';
 import '../../../styles/main/person/FilmographyCategory.css';
 
 const FilmographyCategory = () => {
+    const [show, setShow] = useState(false);
+
     return (
         <section className='filmography_category'>
             <div className='category_left'>
                 <p>{title}</p>
                 <p>{`(${creditsNumber})`}</p>
             </div>
-            <div className='category_right'></div>
+            <div className='category_right'>
+                <p>{show ? 'Show' : 'Hide'}</p>
+                <p>{show ? <ArrowDropUpIcon /> : <ArrowDropDownIcon />}</p>
+            </div>
         </section>
     );
 };
