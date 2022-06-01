e055c8da0a09ecd526b3b896c49f760f7f92fc90
David Robison <drbr56@gmail.com>
2022-06-01 11:22:53 -0600

    Fixed css import path.

--- src/components/main/person/filmography/FilmographyCategory.js
+++ src/components/main/person/filmography/FilmographyCategory.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import FilmographyCard from './FilmographyCard';
-import '../../../styles/main/person/FilmographyCategory.css';
+import '../../../styles/main/person/filmography/FilmographyCategory.css';
 
 const FilmographyCategory = () => {
     const [show, setShow] = useState(false);
