863da294d6aedecb41d59e8c87660cbbcd877b7c
David Robison <drbr56@gmail.com>
2022-06-01 11:42:03 -0600

    Fixed Biography import path.

--- src/components/main/person/PersonMobile.js
+++ src/components/main/person/PersonMobile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Biography from './Biography';
+import Biography from './biography/Biography';
 import PersonImage from './PersonImage';
 import PersonalInformation from './PersonalInformation';
 import KnownFor from './knownFor/KnownFor';
