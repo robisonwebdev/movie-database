ef89e1d3aa74a7a32337fa80c7370a2b1e945414
David Robison <drbr56@gmail.com>
2022-06-02 03:48:41 -0600

    Renamed component and export to Portrait. Fixed css import path. Changed img className to .portrait

--- src/components/main/person/misc/Portrait.js
+++ src/components/main/person/misc/Portrait.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import '../../../styles/main/person/PersonImage.css';
+import '../../../../styles/main/person/misc/Portrait.css';
 
-const PersonImage = ({ format, name, path }) => {
+const Portrait = ({ format, name, path }) => {
     const imageSrc = `https://www.themoviedb.org/t/p/w600_and_h900_bestv2${path}`;
 
     return (
         <>
-            <img className='person_image' src={imageSrc} alt={name} />
+            <img className='portrait' src={imageSrc} alt={name} />
         </>
     );
 };
 
-export default PersonImage;
+export default Portrait;
