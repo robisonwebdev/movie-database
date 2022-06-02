ef3cbeed87cfe17f86e668b6fcc7497796ceea49
David Robison <drbr56@gmail.com>
2022-06-02 03:37:28 -0600

    Replaced PersonSocialMedia component with SocialMedia component.

--- src/components/main/person/misc/Title.js
+++ src/components/main/person/misc/Title.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PersonSocialMedia from './PersonSocialMedia';
+import SocialMedia from './SocialMedia';
 import '../../../../styles/main/person/misc/Title.css';
 
 const Title = ({ format, personInformation }) => {
@@ -8,7 +8,7 @@ const Title = ({ format, personInformation }) => {
     return (
         <div className={format === 'mobile' ? 'person_title_mobile' : 'person_title_desktop'}>
             <h1>{name}</h1>
-            {format === 'mobile' ? <PersonSocialMedia socialMedia={personInformation} /> : null}
+            {format === 'mobile' ? <SocialMedia socialMedia={personInformation} /> : null}
         </div>
     );
 };
