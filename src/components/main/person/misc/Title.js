818967c811207db3495da79ef28daf26e546404a
David Robison <drbr56@gmail.com>
2022-06-02 03:51:56 -0600

    Removed person_ from each return div classNames.

--- src/components/main/person/misc/Title.js
+++ src/components/main/person/misc/Title.js
@@ -6,7 +6,7 @@ const Title = ({ format, personInformation }) => {
     const { name } = personInformation;
 
     return (
-        <div className={format === 'mobile' ? 'person_title_mobile' : 'person_title_desktop'}>
+        <div className={format === 'mobile' ? 'title_mobile' : 'title_desktop'}>
             <h1>{name}</h1>
             {format === 'mobile' ? <SocialMedia socialMedia={personInformation} /> : null}
         </div>
