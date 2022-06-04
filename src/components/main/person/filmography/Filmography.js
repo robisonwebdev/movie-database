e86198280a873b3320ae466f07a6eabce316e733
David Robison <drbr56@gmail.com>
2022-06-04 17:16:25 -0600

    Created new usecallback function(buildFilmographyCatagoies). Take crew objects and sorts into catagories based on department, then calls setFilmography state to update state. Called buildFilmographyCatagories in useEffect.

--- src/components/main/person/filmography/Filmography.js
+++ src/components/main/person/filmography/Filmography.js
@@ -5,9 +5,34 @@ const Filmography = ({ combinedCredits }) => {
     const { cast, crew } = combinedCredits;
     const [filmography, setFilmography] = useState([]);
 
+    const buildFilmographyCatagories = useCallback(() => {
+        const newState = filmography;
+
+        crew.forEach(obj => {
+            const hasdepartment = newState.some(item => item.department === obj.department);
+
+            if (hasdepartment) {
+                const departmentIndex = newState.findIndex(item => item.department === obj.department);
+
+                newState[departmentIndex]['list'].push(obj);
+            };
+            
+            if (!hasdepartment) {
+                let newObject = {
+                    department: obj.department,
+                    list: [obj]
+                };
+
+                newState.push(newObject);
+            };
+        });
+
+        setFilmography(newState);
+    }, [crew, filmography]);
+
     useEffect(() => {
-        
-    }, []);
+        buildFilmographyCatagories();
+    }, [buildFilmographyCatagories]);
 
     return (
         <section className='filmography'>
