71086aa16dd6b70a42a49fa55e5e16bf7c9b7b0a
David Robison <drbr56@gmail.com>
2022-06-04 05:42:32 -0600

    Removed axios import. Removed jobList and loading state. Removed getJobList callback. Removed getJobList from useEffect.

--- src/components/main/person/filmography/Filmography.js
+++ src/components/main/person/filmography/Filmography.js
@@ -1,38 +1,18 @@
 import React, { useCallback, useEffect, useState } from 'react';
-import axios from 'axios';
 import '../../../../styles/main/person/filmography/Filmography.css';
 
 const Filmography = ({ combinedCredits }) => {
     const { cast, crew } = combinedCredits;
     const [filmography, setFilmography] = useState([]);
-    const [jobList, setJobList] = useState([]);
-    const [loading, setLoading] = useState(true);
-
-    const getJobList = useCallback(() => {
-        const jobList_API = `https://api.themoviedb.org/3/configuration/jobs?api_key=9289aca3a6413b200619b263ac82e4c0`;
-
-        setLoading(true);
-
-        axios
-            .get(jobList_API)
-            .then(res => {
-                setJobList(res.data);
-                setLoading(false);
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    }, []);
 
     useEffect(() => {
-        getJobList();
-    }, [getJobList]);
+        
+    }, []);
 
-    console.log(combinedCredits)
     return (
         <section className='filmography'>
             <h2>Filmography</h2>
-            {loading ? null : console.log(crew)}
+            {console.log('Filmography:', filmography)}
         </section>
     );
 };
