188120092d1c7becebab5c72c7e145232e1f17b9
David Robison <drbr56@gmail.com>
2022-06-08 11:54:09 -0600

    Setup getCharacter function to get character and return either null or film.character Added new div and p tags to return section. Called getCharacter in div p tag.

--- src/components/main/person/filmography/FilmographyCard.js
+++ src/components/main/person/filmography/FilmographyCard.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import '../../../../styles/main/person/filmography/FilmographyCard.css';
 
-const FilmographyCard = () => {
+const FilmographyCard = ({ film }) => {
+    const getCharacter = () => {
+        const character = film.character;
+
+        if (character === '') return null;
+
+        return character;
+    };
+
+    const filmYear = () => {
+        const orginalDate = new Date(film.release_date || film.first_air_date);
+        const getYear = orginalDate.getUTCFullYear();
+
+        return getYear;
+    };
+
     return (
-        <section className='filmography_card'>FilmographyCard</section>
+        <section className='filmography_card'>
+            {console.log(film)}
+            <div>
+                <p>{getCharacter()}</p>
+            </div>
+            <p></p>
+        </section>
     );
 };
 
