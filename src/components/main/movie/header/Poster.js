7fad6cb8546ba1126d40751868c17df40809c6d0
David Robison <drbr56@gmail.com>
2022-06-09 05:37:14 -0600

    Imported css. Gave return section a className .movie_header_poster

--- src/components/main/movie/header/Poster.js
+++ src/components/main/movie/header/Poster.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../../../../styles/main/movie/header/Poster.css';
 
 const Poster = () => {
     return (
-        <section>Poster</section>
+        <section className='movie_header_poster'>Poster</section>
     );
 };
 
