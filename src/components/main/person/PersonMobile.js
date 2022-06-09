ff1a191ec1bf960a8d39d9ece6199290f782c486
David Robison <drbr56@gmail.com>
2022-06-09 04:33:31 -0600

    Imported Filmography component. Added Filmography component to .person_mobile

--- src/components/main/person/PersonMobile.js
+++ src/components/main/person/PersonMobile.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import Biography from './biography/Biography';
-import Portrait from './misc/Portrait';
-import PersonalInformation from './personalInformation/PersonalInformation';
+import Filmography from './filmography/Filmography';
 import KnownFor from './knownFor/KnownFor';
+import PersonalInformation from './personalInformation/PersonalInformation';
+import Portrait from './misc/Portrait';
 import Title from './misc/Title';
 import '../../../styles/main/person/PersonMobile.css';
 
@@ -16,6 +17,7 @@ const PersonMobile = ({ personInformation }) => {
             <Biography biography={biography} format='mobile' />
             <PersonalInformation format='mobile' personalInformation={personInformation} />
             <KnownFor credits={combinedCredits.cast} format='mobile' />
+            <Filmography combinedCredits={combinedCredits} />
         </section>
     );
 };
