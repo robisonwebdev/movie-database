cabe1b641f567f89959f8977d40d15caecbc9537
David Robison <drbr56@gmail.com>
2022-06-09 05:09:49 -0600

    Imported Link from react-router-dom. Setup linkTo variable to contains link path. Surrounded img with Link component and passed it linkTo. Surrounded name || title with Link component and passed it linkTo.

--- src/components/main/person/knownFor/KnownForCard.js
+++ src/components/main/person/knownFor/KnownForCard.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import MovieIcon from '@mui/icons-material/Movie';
 import '../../../../styles/main/person/knownFor/KnownForCard.css';
 
 const KnownForCard = ({ details, format }) => {
-    const { name, poster_path, title} = details;
+    const { id, name, poster_path, title} = details;
+
+    const linkTo = `/movie/${id}`;
 
     const getPoster = () => {
         if (poster_path === null || poster_path === '') return <MovieIcon />;
 
-        return <img src={`https://www.themoviedb.org/t/p/w150_and_h225_bestv2${poster_path}`} alt={name || title} />
+        return (
+            <Link to={linkTo}>
+                <img src={`https://www.themoviedb.org/t/p/w150_and_h225_bestv2${poster_path}`} alt={name || title} />
+            </Link>
+        );
     };
 
     return (
@@ -16,7 +23,11 @@ const KnownForCard = ({ details, format }) => {
             <div className='card_poster'>
                 {getPoster()}
             </div>
-            <p>{name || title }</p>
+            <p>
+                <Link to={linkTo}>
+                    {name || title}
+                </Link>
+            </p>
         </section>
     );
 };
