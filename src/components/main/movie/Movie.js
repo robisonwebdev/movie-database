e193cc2a7cb6b55535184b5bb6a368f329fafa16
David Robison <drbr56@gmail.com>
2022-06-12 11:20:39 -0600

    Setup state for loading, and set to an initial value of true. Setup state for movieData, and set to an initial value of empty array. Setup fetchData useCallback, that take movieID and uses axios to get data on movie. Setup useEffect to run fetchData. Added console.logs for testing.

--- src/components/main/movie/Movie.js
+++ src/components/main/movie/Movie.js
@@ -5,10 +5,35 @@ import '../../../styles/main/movie/Movie.css';
 
 const Movie = () => {
     const { movieID } = useParams();
+    const [loading, setLoading ] = useState(true);
+    const [movieData, setMovieData] = useState([]);
+
+    const fetchData = useCallback(() => {
+        const movie_API = `https://api.themoviedb.org/3/movie/${movieID}?api_key=9289aca3a6413b200619b263ac82e4c0&language=en-US`;
+
+        const getMovie = axios.get(movie_API);
+
+        setLoading(true);
+
+        axios
+        .all([getMovie])
+        .then(axios.spread((...all_Data) => {
+            const movie_Data = all_Data[0].data;
+
+            setMovieData(movie_Data);
+
+            setLoading(false);
+        }))
+    }, [movieID]);
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
 
     return (
         <section className='movie'>
-            Movie
+            {loading ? null : console.log('MovieData:', movieData)}
+            {loading ? null : movieData.title}
         </section>
     );
 };
