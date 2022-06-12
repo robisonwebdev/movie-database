e6d4a9bb10307971bd4e71f027f357deb182f5ee
David Robison <drbr56@gmail.com>
2022-06-12 11:26:23 -0600

    Imported MovieDesktop.css Gave return section a className of .movie_desktop

--- src/components/main/movie/MovieDesktop.js
+++ src/components/main/movie/MovieDesktop.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../../../styles/main/movie/MovieDesktop.css';
 
 const MovieDesktop = () => {
     return (
-        <section>MovieDesktop</section>
+        <section className='movie_desktop'>MovieDesktop</section>
     );
 };
 
