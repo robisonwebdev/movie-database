cdd99b8ce6e8ffb57eb240aa7338e29b63ef0c60
David Robison <drbr56@gmail.com>
2022-06-12 11:08:17 -0600

    Imported useCallback, useEffect, useState, and axios.

--- src/components/main/movie/Movie.js
+++ src/components/main/movie/Movie.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import '../../../styles/main/movie/Movie.css';
 
 const Movie = () => {
