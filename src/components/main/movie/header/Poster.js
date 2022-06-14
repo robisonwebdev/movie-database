93a9a9bbbeebd035c91e9bb496c76cd5b0ff851d
David Robison <drbr56@gmail.com>
2022-06-14 08:47:52 -0600

    Destructured format and movie. Destructured backdrop_path and poster_path from movie. Setup two new empty functions called desktopDisplay and mobileDisplay.

--- src/components/main/movie/header/Poster.js
+++ src/components/main/movie/header/Poster.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import '../../../../styles/main/movie/header/Poster.css';
 
-const Poster = () => {
+const Poster = ({ format, movie }) => {
+    const { backdrop_path, poster_path } = movie;
+    
+    const desktopDisplay = () => {};
+
+    const mobileDisplay = () => {};
+
     return (
         <section className='movie_header_poster'>Poster</section>
     );
