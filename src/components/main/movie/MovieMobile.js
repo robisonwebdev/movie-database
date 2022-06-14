769a744a37220e82f8df4364c928985933fba873
David Robison <drbr56@gmail.com>
2022-06-14 08:35:05 -0600

    Imported Poster component. Destructured movie. Called Poster component in movie_mobile_header

--- src/components/main/movie/MovieMobile.js
+++ src/components/main/movie/MovieMobile.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Poster from './header/Poster';
 import '../../../styles/main/movie/MovieMobile.css';
 
-const MovieMobile = () => {
+const MovieMobile = ({ movie }) => {
     return (
         <section className='movie_mobile'>
-            <section className='movie_mobile_header'>Header</section>
+            <section className='movie_mobile_header'>
+                <Poster />
+            </section>
             <section className='movie_mobile_main'>Main</section>
         </section>
     );
