7b5d2e73c0251844a136807e9038911ea2efdbf7
David Robison <drbr56@gmail.com>
2022-06-14 08:32:53 -0600

    Added two new sections in .movie_mobile, and gave them a className of .movie_mobile_header and .movie_mobile_main

--- src/components/main/movie/MovieMobile.js
+++ src/components/main/movie/MovieMobile.js
@@ -3,7 +3,10 @@ import '../../../styles/main/movie/MovieMobile.css';
 
 const MovieMobile = () => {
     return (
-        <section className='movie_mobile'>MovieMobile</section>
+        <section className='movie_mobile'>
+            <section className='movie_mobile_header'>Header</section>
+            <section className='movie_mobile_main'>Main</section>
+        </section>
     );
 };
 
