79913846c6c7bd78c39db55c6ed7a2ef370418a1
David Robison <drbr56@gmail.com>
2022-06-16 13:14:38 -0600

    Destructured genres from movie. Imported Genres component. Called Genres component below Date component. Passed genres prop down to Genres component.

--- src/components/main/movie/title/Title.js
+++ src/components/main/movie/title/Title.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Date from './Date';
+import Genres from './Genres';
 import Rating from './Rating';
 import '../../../../styles/main/movie/title/Title.css';
 
 const Title = ({ movie }) => {
-    const { release_dates, title } = movie;
+    const { genres, release_dates, title } = movie;
 
     return (
         <section className='movie_header_title'>
@@ -13,6 +14,7 @@ const Title = ({ movie }) => {
                 <div>
                     <Rating data={release_dates} />
                     <Date data={release_dates} />
+                    <Genres data={genres} />
                 </div>
             </div>
         </section>
