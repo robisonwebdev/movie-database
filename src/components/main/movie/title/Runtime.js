1c9aa60782c85875010d893c3b0b417c19689fbc
David Robison <drbr56@gmail.com>
2022-06-16 13:17:23 -0600

    Destructured data prop.

--- src/components/main/movie/title/Runtime.js
+++ src/components/main/movie/title/Runtime.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../../../styles/main/movie/title/Runtime.css';
 
-const Runtime = () => {
+const Runtime = ({ data }) => {
     return (
         <div>Runtime</div>
     );
