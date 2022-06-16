ddf1c71f2a0d13289d128a72f66193b476d08a70
David Robison <drbr56@gmail.com>
2022-06-16 13:18:52 -0600

    Imported Runtime component. Destructured runtime from movie. Called Runtime component below Genres component. Passed runtime prop downt to Runtime component.

--- src/components/main/movie/title/Title.js
+++ src/components/main/movie/title/Title.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Date from './Date';
 import Genres from './Genres';
 import Rating from './Rating';
+import Runtime from './Runtime';
 import '../../../../styles/main/movie/title/Title.css';
 
 const Title = ({ movie }) => {
-    const { genres, release_dates, title } = movie;
+    const { genres, release_dates, runtime, title } = movie;
 
     return (
         <section className='movie_header_title'>
@@ -15,6 +16,7 @@ const Title = ({ movie }) => {
                     <Rating data={release_dates} />
                     <Date data={release_dates} />
                     <Genres data={genres} />
+                    <Runtime data={runtime} />
                 </div>
             </div>
         </section>
