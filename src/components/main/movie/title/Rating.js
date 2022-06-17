f6872c92ae7f0a1f64dae7e04ec9d58c9b2b96dd
David Robison <drbr56@gmail.com>
2022-06-17 09:46:34 -0600

    Setup USReleaseDates variable to filter data looking for US release dates.

--- src/components/main/movie/title/Rating.js
+++ src/components/main/movie/title/Rating.js
@@ -2,8 +2,11 @@ import React from 'react';
 import '../../../../styles/main/movie/title/Rating.css';
 
 const Rating = ({ data }) => {
+    const USReleaseDates = data.results.filter(obj => obj['iso_3166_1'] === 'US');
     return (
-        <div>Rating</div>
+        <div>
+             {console.log('RD_Data:', USReleaseDates)}
+        </div>
     );
 };
 
