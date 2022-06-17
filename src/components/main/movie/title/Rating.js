9cd654dca2956755da4bf4910be20d4baf248c19
David Robison <drbr56@gmail.com>
2022-06-17 10:04:52 -0600

    Setup theatricalReleaseDates to filter USRealseDates, looking for type to equal 3. Setup getRating function to get movie rating from theatricalReleaseDates.

--- src/components/main/movie/title/Rating.js
+++ src/components/main/movie/title/Rating.js
@@ -3,9 +3,20 @@ import '../../../../styles/main/movie/title/Rating.css';
 
 const Rating = ({ data }) => {
     const USReleaseDates = data.results.filter(obj => obj['iso_3166_1'] === 'US');
+    const theatricalReleaseDates = USReleaseDates[0]['release_dates'].filter(obj => obj.type === 3);
+    const movieRating = theatricalReleaseDates[0]['certification'];
+
+    const getRating = () => {
+        const findRating = theatricalReleaseDates.find(({ certification }) => certification !== '');
+
+        if (findRating === undefined) return 'NA';
+
+        return findRating['certification'];
+    };
+
     return (
         <div>
-             {console.log('RD_Data:', USReleaseDates)}
+             {console.log('RD_Data:', getRating())}
         </div>
     );
 };
