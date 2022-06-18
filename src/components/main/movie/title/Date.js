db1619f28c578a9ee3aa175f6e014c358ed9133b
David Robison <drbr56@gmail.com>
2022-06-18 13:27:55 -0600

    Setup getDate function to find release date from data and return date in month/day/year format. Changed component/export name from Date to MovieDate to avoid error.

--- src/components/main/movie/title/Date.js
+++ src/components/main/movie/title/Date.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import '../../../../styles/main/movie/title/Date.css';
 
-const Date = ({ data }) => {
+const MovieDate = ({ data }) => {
     const USReleaseDates = data.results.filter(obj => obj['iso_3166_1'] === 'US');
     const theatricalReleaseDates = USReleaseDates[0]['release_dates'].filter(obj => obj.type === 3);
+
+    const getDate = () => {
+        const findReleaseDate = theatricalReleaseDates.find(({ release_date }) => release_date)
+        const getDate = new Date(findReleaseDate['release_date']);
+        const year = getDate.getUTCFullYear();
+        const month = getDate.getUTCMonth() + 1;
+        const day = getDate.getUTCDate();
+
+        return `${month}/${day}/${year} (US)`;
+    };
 
     return (
         <div>
             {console.log(theatricalReleaseDates)}
+            {getDate()}
         </div>
     );
 };
 
-export default Date;
+export default MovieDate;
