e57ae8c2f782468eaefa4a6f3e3a7a9f03dc770b
David Robison <drbr56@gmail.com>
2022-06-18 13:13:50 -0600

    Setup USReleaseDates variable to filter out data for iso_3166_1 equals US. Setup theatricalReleaseDates to filter out USReleaseDates for type equals 3.

--- src/components/main/movie/title/Date.js
+++ src/components/main/movie/title/Date.js
@@ -2,8 +2,13 @@ import React from 'react';
 import '../../../../styles/main/movie/title/Date.css';
 
 const Date = ({ data }) => {
+    const USReleaseDates = data.results.filter(obj => obj['iso_3166_1'] === 'US');
+    const theatricalReleaseDates = USReleaseDates[0]['release_dates'].filter(obj => obj.type === 3);
+
     return (
-        <div>Date</div>
+        <div>
+            {console.log(theatricalReleaseDates)}
+        </div>
     );
 };
 
