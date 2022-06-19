80e779b3a03763ff6093984632f0084659707693
David Robison <drbr56@gmail.com>
2022-06-19 11:18:10 -0600

    Repalced destructured data prop with runtime prop. Setup computeRuntime function to take runtime prop, and convert number into hours and mins. Changed return div to p tag. Gave return p tag a className of .movie_runtime Called computeRuntime in .movie_runtime

--- src/components/main/movie/title/Runtime.js
+++ src/components/main/movie/title/Runtime.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import '../../../../styles/main/movie/title/Runtime.css';
 
-const Runtime = ({ data }) => {
+const Runtime = ({ runtime }) => {
+    const computeRuntime = () => {
+        const runtimeToHours = (runtime / 60);
+        const hours = Math.floor(runtimeToHours);
+        const minutes = Math.floor((runtimeToHours - hours) * 60);
+
+        return hours >=1 ? `${hours}h ${minutes}m` : `${minutes}m`;
+    };
+
     return (
-        <div>Runtime</div>
+        <p className='movie_runtime'>
+            {computeRuntime()}
+        </p>
     );
 };
 
