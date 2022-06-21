a7889e4e917accd817d29b019d98d18b9b924b55
David Robison <drbr56@gmail.com>
2022-06-21 09:00:19 -0600

    Destructured movie prop. Destructured overview from movie. Added temp console.log to return section.

--- src/components/main/movie/overview/Overview.js
+++ src/components/main/movie/overview/Overview.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import '../../../../styles/main/movie/overview/Overview.css';
 
-const Overview = () => {
+const Overview = ({ movie }) => {
+    const { overview } = movie;
+
     return (
-        <section className='movie_header_overview'>Overview</section>
+        <section className='movie_header_overview'>
+            {console.log(overview)}
+        </section>
     );
 };
 
