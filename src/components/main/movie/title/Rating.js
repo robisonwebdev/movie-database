cb95c392563c05b8d214a30b69c7e28fb85115c8
David Robison <drbr56@gmail.com>
2022-06-21 11:07:14 -0600

    Fixed component's getRating to account for USReleaseDates having an empty array that causes program to crash.

--- src/components/main/movie/title/Rating.js
+++ src/components/main/movie/title/Rating.js
@@ -3,14 +3,18 @@ import '../../../../styles/main/movie/title/Rating.css';
 
 const Rating = ({ data }) => {
     const USReleaseDates = data.results.filter(obj => obj['iso_3166_1'] === 'US');
-    const theatricalReleaseDates = USReleaseDates[0]['release_dates'].filter(obj => obj.type === 3);
+    let theatricalReleaseDates;
+    let findRating;
 
     const getRating = () => {
-        const findRating = theatricalReleaseDates.find(({ certification }) => certification !== '');
+        if (USReleaseDates.length === 0) {
+            findRating = undefined;
+        } else {
+            theatricalReleaseDates = USReleaseDates[0]['release_dates'].filter(obj => obj.type === 3);
+            findRating = theatricalReleaseDates.find(({ certification }) => certification !== '');
+        };
 
-        if (findRating === undefined) return 'NA';
-
-        return findRating['certification'];
+        return findRating === undefined ? 'NA' : findRating['certification'];
     };
 
     return (
