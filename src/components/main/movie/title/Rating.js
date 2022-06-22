3251f4f5ca00f02c3ecf6a04d6a29965b7767b82
David Robison <drbr56@gmail.com>
2022-06-22 10:36:15 -0600

    Changed data prop to releaseDate. Removed USReleaseDates, theatricalReleaseDates and findRating variables. Rework getRating to use less code and look cleaner. Replaced getRating call in .movie_rating, with a ternary that checks releaseDate prop and return NA or calls getRating.

--- src/components/main/movie/title/Rating.js
+++ src/components/main/movie/title/Rating.js
@@ -1,25 +1,16 @@
 import React from 'react';
 import '../../../../styles/main/movie/title/Rating.css';
 
-const Rating = ({ data }) => {
-    const USReleaseDates = data.results.filter(obj => obj['iso_3166_1'] === 'US');
-    let theatricalReleaseDates;
-    let findRating;
-
+const Rating = ({ releaseDate }) => {
     const getRating = () => {
-        if (USReleaseDates.length === 0) {
-            findRating = undefined;
-        } else {
-            theatricalReleaseDates = USReleaseDates[0]['release_dates'].filter(obj => obj.type === 3);
-            findRating = theatricalReleaseDates.find(({ certification }) => certification !== '');
-        };
+        const rating = releaseDate.find(({ certification }) => certification !== '');
 
-        return findRating === undefined ? 'NA' : findRating['certification'];
+        return rating === undefined ? 'NA' : rating['certification'];
     };
 
     return (
         <p className='movie_rating'>
-             {getRating()}
+             {releaseDate === null ? 'NA' : getRating()}
         </p>
     );
 };
