f656fe1bbd4864e123f3520cabe4af0a3efbaffe
David Robison <drbr56@gmail.com>
2022-06-23 04:16:46 -0600

    Destructured isMobile. Passed isMobile down to MovieHeader.

--- src/components/main/movie/MovieMobile.js
+++ src/components/main/movie/MovieMobile.js
@@ -2,10 +2,10 @@ import React from 'react';
 import MovieHeader from './header/MovieHeader';
 import '../../../styles/main/movie/MovieMobile.css';
 
-const MovieMobile = ({ movie }) => {
+const MovieMobile = ({ isMobile, movie }) => {
     return (
         <section className='movie_mobile'>
-            <MovieHeader movie={movie} />
+            <MovieHeader isMobile={isMobile} movie={movie} />
             <section className='movie_mobile_main'>Main</section>
         </section>
     );
