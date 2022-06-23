47026ee1c78a1e4e978f951ffc477debe053369f
David Robison <drbr56@gmail.com>
2022-06-23 03:51:58 -0600

    Imported MoviePosterDesktop component. Added new section with a className of .movie_header_desktop to return section.

--- src/components/main/movie/MovieDesktop.js
+++ src/components/main/movie/MovieDesktop.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import MoviePosterDesktop from './poster/MoviePosterDesktop';
 import '../../../styles/main/movie/MovieDesktop.css';
 
 const MovieDesktop = () => {
     return (
-        <section className='movie_desktop'>MovieDesktop</section>
+        <section className='movie_desktop'>
+            <section className='movie_header_desktop'></section>
+        </section>
     );
 };
 
