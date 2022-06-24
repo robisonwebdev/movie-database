cc2448f179ef9cc75262893c9df066c80525eff8
David Robison <drbr56@gmail.com>
2022-06-24 15:31:31 -0600

    Called Poster component in .left_header

--- src/components/main/movie/header/MovieHeader.js
+++ src/components/main/movie/header/MovieHeader.js
@@ -15,7 +15,9 @@ const MovieHeader = ({ isMobile, movie }) => {
 const DesktopLayout = ({ isMobile, movie }) => {
     return (
         <section className='movie_header_desktop'>
-            <section className='left_header'></section>
+            <section className='left_header'>
+                <Poster isMobile={isMobile} movie={movie} />
+            </section>
             <section className='right_header'></section>
         </section>
     );
