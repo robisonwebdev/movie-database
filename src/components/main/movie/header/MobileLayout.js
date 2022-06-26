1e0f3621b25aaad844cbd72c5c6a74f5ec6b179e
David Robison <drbr56@gmail.com>
2022-06-26 14:07:09 -0600

    Imported Overview, Poster, and Title components. Imported MobileLayout.css

--- src/components/main/movie/header/MobileLayout.js
+++ src/components/main/movie/header/MobileLayout.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Overview from '../overview/Overview';
+import Poster from '../poster/Poster';
+import Title from '../title/Title';
+import '../../../../styles/main/movie/header/MobileLayout.css'
 
 const MobileLayout = ({ isMobile, movie }) => {
     return (
