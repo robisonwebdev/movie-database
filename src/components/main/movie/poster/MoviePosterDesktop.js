38deedb3e3d71a8bad4f3c3ab666ef5468d81797
David Robison <drbr56@gmail.com>
2022-06-26 13:38:40 -0600

    Imported MovieIcon. Destrucutured movie prop. Destructured poster_path and title from movie. Setup getPoster function that gets posterPath using poster_path. Returns either MovieIcon if poster_path is null or img tag. Called getPoster in .movie_poster_desktop

--- src/components/main/movie/poster/MoviePosterDesktop.js
+++ src/components/main/movie/poster/MoviePosterDesktop.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import MovieIcon from '@mui/icons-material/Movie';
 import '../../../../styles/main/movie/poster/MoviePosterDesktop.css';
 
-const MoviePosterDesktop = () => {
+const MoviePosterDesktop = ({ movie }) => {
+    const { poster_path, title } = movie;
+
+    const getPoster = () => {
+        const posterPath = `https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${poster_path}`;
+        
+        if (poster_path === null) return <MovieIcon />
+
+        return <img src={posterPath} alt={title} />
+    };
+
     return (
-        <section className='movie_poster_desktop'>MoviePosterDesktop</section>
+        <section className='movie_poster_desktop'>
+            {getPoster()}
+        </section>
     );
 };
 
