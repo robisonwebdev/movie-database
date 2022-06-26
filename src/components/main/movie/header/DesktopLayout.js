f9c346c04ac64cdb71b63127ef786fb8d1a70222
David Robison <drbr56@gmail.com>
2022-06-26 13:59:31 -0600

    Removed DesktopLayout from MovieHeader.js and moved it to its own component.

--- src/components/main/movie/header/DesktopLayout.js
+++ src/components/main/movie/header/DesktopLayout.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
-const DesktopLayout = () => {
+const DesktopLayout = ({ isMobile, movie }) => {
     return (
-        <section>DesktopLayout</section>
+        <section className='movie_header_desktop'>
+            <section className='left_header'>
+                <Poster isMobile={isMobile} movie={movie} />
+            </section>
+            <section className='right_header'>
+                <Title movie={movie} />
+                <Overview movie={movie} />
+            </section>
+        </section>
     );
 };
 
--- src/components/main/movie/header/MovieHeader.js
+++ src/components/main/movie/header/MovieHeader.js
@@ -12,20 +12,6 @@ const MovieHeader = ({ isMobile, movie }) => {
     );
 };
 
-const DesktopLayout = ({ isMobile, movie }) => {
-    return (
-        <section className='movie_header_desktop'>
-            <section className='left_header'>
-                <Poster isMobile={isMobile} movie={movie} />
-            </section>
-            <section className='right_header'>
-                <Title movie={movie} />
-                <Overview movie={movie} />
-            </section>
-        </section>
-    );
-};
-
 const MobileLayout = ({ isMobile, movie }) => {
     return (
         <section className='movie_header_mobile'>
