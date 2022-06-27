1645cd2630d33c9e18ac3ce7a481dda283b98c29
David Robison <drbr56@gmail.com>
2022-06-27 09:29:59 -0600

    Setup headerBackground function to get backdrop_path from movie, call URL, and return styling for image. Called headerBackground in style event on .movie_header

--- src/components/main/movie/header/MovieHeader.js
+++ src/components/main/movie/header/MovieHeader.js
@@ -4,8 +4,21 @@ import MobileLayout from './MobileLayout';
 import '../../../../styles/main/movie/header/MovieHeader.css';
 
 const MovieHeader = ({ isMobile, movie }) => {
+    const headerBackground = () => {
+        const { backdrop_path } = movie;
+        const backgroundURL = `http://image.tmdb.org/t/p/original/${backdrop_path}`;
+        const styling = {
+            background: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.9)), url(${backgroundURL})`,
+            backgroundPosition: 'center top',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover'
+        };
+
+        return styling;
+    };
+
     return (
-        <section className='movie_header'>
+        <section className='movie_header' style={headerBackground()}>
             {isMobile ? <MobileLayout isMobile={isMobile} movie={movie} /> : <DesktopLayout isMobile={isMobile} movie={movie} />}
         </section>
     );
