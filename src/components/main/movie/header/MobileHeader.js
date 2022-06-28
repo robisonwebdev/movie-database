9e9cc3a2f89091beb63529d1c2f421d9b328f627
David Robison <drbr56@gmail.com>
2022-06-28 11:00:19 -0600

    Removed Poster component.

--- src/components/main/movie/header/MobileHeader.js
+++ src/components/main/movie/header/MobileHeader.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Overview from '../overview/Overview';
-import Poster from '../poster/Poster';
 import Title from '../title/Title';
 import '../../../../styles/main/movie/header/MobileHeader.css'
 
 const MobileHeader = ({ isMobile, movie }) => {
     return (
         <section className='movie_header_mobile'>
-            <Poster isMobile={isMobile} movie={movie} />
             <Title movie={movie} />
             <Overview movie={movie} />
         </section>
