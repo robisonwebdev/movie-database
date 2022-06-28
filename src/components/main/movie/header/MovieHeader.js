cf0879cd21fbef72e87e5e6d9f26dd70bb6c6357
David Robison <drbr56@gmail.com>
2022-06-28 10:49:54 -0600

    Component to be deleted.

--- src/components/main/movie/header/MovieHeader.js
+++ src/components/main/movie/header/MovieHeader.js
@@ -1,24 +1,12 @@
 import React from 'react';
+import headerBackground from './backgroundStyling';
 import DesktopLayout from './DesktopLayout';
 import MobileLayout from './MobileLayout';
 import '../../../../styles/main/movie/header/MovieHeader.css';
 
 const MovieHeader = ({ isMobile, movie }) => {
-    const headerBackground = () => {
-        const { backdrop_path } = movie;
-        const backgroundURL = `http://image.tmdb.org/t/p/original/${backdrop_path}`;
-        const styling = {
-            background: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.9)), url(${backgroundURL})`,
-            backgroundPosition: 'center top',
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover'
-        };
-
-        return styling;
-    };
-
     return (
-        <section className='movie_header' style={headerBackground()}>
+        <section className='movie_header'>
             {isMobile ? <MobileLayout isMobile={isMobile} movie={movie} /> : <DesktopLayout isMobile={isMobile} movie={movie} />}
         </section>
     );
