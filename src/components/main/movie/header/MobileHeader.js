7b91f9a049c554cd417761a3d444dc5d73c8d659
David Robison <drbr56@gmail.com>
2022-06-28 11:10:25 -0600

    Imported headerBackground. Added empty div to return section. Gave div a className of .mobile_header and passed headerBackground to style event.

--- src/components/main/movie/header/MobileHeader.js
+++ src/components/main/movie/header/MobileHeader.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import headerBackground from './backgroundStyling';
 import Overview from '../overview/Overview';
 import Title from '../title/Title';
 import '../../../../styles/main/movie/header/MobileHeader.css'
@@ -6,6 +7,7 @@ import '../../../../styles/main/movie/header/MobileHeader.css'
 const MobileHeader = ({ isMobile, movie }) => {
     return (
         <section className='movie_header_mobile'>
+            <div className='mobile_background' style={headerBackground(movie)}></div>
             <Title movie={movie} />
             <Overview movie={movie} />
         </section>
