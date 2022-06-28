264d4e3932412b85aff1eb4cb9af5854c5aedc55
David Robison <drbr56@gmail.com>
2022-06-28 08:52:14 -0600

    Destructured movie from component prop. Destructured genres, release_dates, runtime, and title from movie. Setup state for releaseDate, with an initial value of null. Setup useEffect to handle setReleaseDate, by filtering release_dates looking for a certain release_date type.

--- src/components/main/movie/facts/Facts.js
+++ src/components/main/movie/facts/Facts.js
@@ -5,7 +5,20 @@ import Rating from './Rating';
 import Runtime from './Runtime';
 import '../../../../styles/main/movie/facts/Facts.css';
 
-const Facts = () => {
+const Facts = ({ movie }) => {
+    const { genres, release_dates, runtime, title } = movie;
+    const [releaseDate, setReleaseDate] = useState(null);
+
+    useEffect(() => {
+        const USReleaseDates = release_dates.results.filter(obj => obj['iso_3166_1'] === 'US');
+
+        if (USReleaseDates.length !== 0) {
+            let movieDates = USReleaseDates[0]['release_dates'].filter(obj => obj.type === 3);
+
+            setReleaseDate(movieDates);
+        };
+    }, [release_dates]);
+    
     return (
         <section className='movie_facts'>Facts</section>
     );
