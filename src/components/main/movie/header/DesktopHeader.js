935d572b658c40b1a04e7e893a44ded23c38520c
David Robison <drbr56@gmail.com>
2022-06-28 10:54:41 -0600

    Changed DesktopLayout text to DesktopHeader.

--- src/components/main/movie/header/DesktopHeader.js
+++ src/components/main/movie/header/DesktopHeader.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import headerBackground from './backgroundStyling';
 import Facts from '../facts/Facts';
 import Overview from '../overview/Overview';
 import Poster from '../poster/Poster';
 import Title from '../title/Title';
-import '../../../../styles/main/movie/header/DesktopLayout.css';
+import '../../../../styles/main/movie/header/DesktopHeader.css';
 
-const DesktopLayout = ({ isMobile, movie }) => {
+const DesktopHeader = ({ isMobile, movie }) => {
     return (
-        <section className='movie_header_desktop'>
+        <section className='movie_header_desktop' style={headerBackground(movie)}>
             <section className='left_header'>
                 <Poster isMobile={isMobile} movie={movie} />
             </section>
@@ -22,4 +23,4 @@ const DesktopLayout = ({ isMobile, movie }) => {
     );
 };
 
-export default DesktopLayout;
+export default DesktopHeader;
