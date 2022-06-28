3006edb63bc50c73bdbaa07f9c37ba5e921e99f1
David Robison <drbr56@gmail.com>
2022-06-28 09:00:04 -0600

    Removed useEffect, useState, Genres, Rating, and Runtime imports. Changed import path for MovieDate. Removed genres, release_dates, and runtime props. Removed releaseDate state. Removed useEffect call. Changed (incorrectly) return section className to .movietitle Removed .movie_title div, but kept its children, Removed .movie_mini_info and its children.

--- src/components/main/movie/title/Title.js
+++ src/components/main/movie/title/Title.js
@@ -1,39 +1,14 @@
-import React, { useEffect, useState } from 'react';
-import MovieDate from './MovieDate';
-import Genres from './Genres';
-import Rating from './Rating';
-import Runtime from './Runtime';
+import React from 'react';
+import MovieDate from '../facts/MovieDate';
 import '../../../../styles/main/movie/title/Title.css';
 
 const Title = ({ movie }) => {
-    const { genres, release_dates, runtime, title } = movie;
-    const [releaseDate, setReleaseDate] = useState(null);
-
-    useEffect(() => {
-        const USReleaseDates = release_dates.results.filter(obj => obj['iso_3166_1'] === 'US');
-
-        if (USReleaseDates.length !== 0) {
-            let movieDates = USReleaseDates[0]['release_dates'].filter(obj => obj.type === 3);
-
-            setReleaseDate(movieDates);
-        };
-    }, [release_dates]);
+    const { title } = movie;
 
     return (
-        <section className='movie_header_title'>
-            <div className='movie_title'>
-                <h3>{title}</h3>
-                <MovieDate releaseDate={releaseDate} format='yyyy' />
-            </div>
-            <div className='movie_mini_info'>
-                <Rating releaseDate={releaseDate} />
-                &#8226;
-                <MovieDate releaseDate={releaseDate} />
-                &#8226;
-                <Genres genres={genres} />
-                &#8226;
-                <Runtime runtime={runtime} />
-            </div>
+        <section className='movietitle'>
+            <h3>{title}</h3>
+            <MovieDate releaseDate={releaseDate} format='yyyy' />
         </section>
     );
 };
