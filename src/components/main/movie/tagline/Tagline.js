f5f95eea0378be33090e166f121702e36cd2911f
David Robison <drbr56@gmail.com>
2022-06-29 11:42:58 -0600

    Imported Tagline.css Destructured movie from component. Destructured tagline from movie.

--- src/components/main/movie/tagline/Tagline.js
+++ src/components/main/movie/tagline/Tagline.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import '../../../../styles/main/movie/tagline/Tagline.css';
 
-const Tagline = () => {
+const Tagline = ({ movie }) => {
+    const { tagline } = movie;
+
     return (
         <h4>Tagline</h4>
     );
 };
 
-export default Tagline;
+export default Tagline; 
