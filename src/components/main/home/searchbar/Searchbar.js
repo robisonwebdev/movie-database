526bba9571b24f0f7cc9a79609eb696ce7d549f1
David Robison <drbr56@gmail.com>
2022-06-30 10:05:30 -0600

    Setup navigate variable for useNavigate. Setup placeholder variable to store text. Setup handleInputChange function to update searchValue. Setup input's type, value, placeholder, and onChange.

--- src/components/main/home/searchbar/Searchbar.js
+++ src/components/main/home/searchbar/Searchbar.js
@@ -4,11 +4,17 @@ import '../../../../styles/main/home/searchbar/Searchbar.css';
 
 const Searchbar = () => {
     const [searchValue, setSearchValue] = useState('');
+    const navigate = useNavigate();
+    const placeholder = 'Search for a movie, tv show, person.....';
+
+    const handleInputChange = (event) => {
+        setSearchValue(event.target.value);
+    };
 
     return (
         <section className='searchbar_container'>
             <form className='searchbar'>
-                <input />
+                <input type='text' value={searchValue} placeholder={placeholder} onChange={handleInputChange} />
                 <button>Search</button>
             </form>
         </section>
