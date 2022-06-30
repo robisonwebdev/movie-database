34c70e5beb5b64d57b0bbbd3d2f70214e92d11dd
David Robison <drbr56@gmail.com>
2022-06-30 09:57:45 -0600

    Imported useState and useNavigate. Setup state for searchValue, and gave an initial value of empty string. Changed return section className to .searchbar_container Added form to .searchbar_container, and gave a className of .searchbar Added input and button to form.

--- src/components/main/home/searchbar/Searchbar.js
+++ src/components/main/home/searchbar/Searchbar.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom"
 import '../../../../styles/main/home/searchbar/Searchbar.css';
 
 const Searchbar = () => {
+    const [searchValue, setSearchValue] = useState('');
+
     return (
-        <section className='home_searchbar'>Searchbar</section>
+        <section className='searchbar_container'>
+            <form className='searchbar'>
+                <input />
+                <button>Search</button>
+            </form>
+        </section>
     );
 };
 
