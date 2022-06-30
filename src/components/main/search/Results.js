c240f0161ef706cc88f9af87de5d7a8c61e7450c
David Robison <drbr56@gmail.com>
2022-06-30 10:19:07 -0600

    Imported useParams. Destrucutured searchValue from useParams.

--- src/components/main/search/Results.js
+++ src/components/main/search/Results.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import '../../../styles/main/search/Results.css';
 
 const Results = () => {
+    const { searchValue } = useParams();
+    
     return (
         <section className='results'>Results</section>
     );
