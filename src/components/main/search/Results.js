697f3135f8133f9a2cda994d47121e29e265b4ee
David Robison <drbr56@gmail.com>
2022-07-01 08:23:44 -0600

    Imported useCallback, useEffect, and useState. Setup initial state for movieResults, poepleResults, and showsResult. Gave all an initial value of an empty array. Setup blank fetchData useCallback. Setup blank useEffect.

--- src/components/main/search/Results.js
+++ src/components/main/search/Results.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import '../../../styles/main/search/Results.css';
 
 const Results = () => {
     const { searchValue } = useParams();
+    const [movieResults, setMovieResults] = useState([]);
+    const [peopleResults, setPeopleResults] = useState([]);
+    const [showsResults, setShowsResults] = useState([]);
+
+    const fetchData = useCallback(() => {
+
+    }, []);
+
+    useEffect(() => {
+        
+    }, []);
     
     return (
         <section className='results'>Results</section>
