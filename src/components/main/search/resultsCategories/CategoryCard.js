7afcdc99e00b12ae100f27cf12075f34e50ee2c4
David Robison <drbr56@gmail.com>
2022-07-02 11:01:50 -0600

    Imported CategoryCard.css Gave return div a className of .category_card

--- src/components/main/search/resultsCategories/CategoryCard.js
+++ src/components/main/search/resultsCategories/CategoryCard.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../../../../styles/main/search/resultsCategories/CategoryCard.css';
 
 const CategoryCard = () => {
     return (
-        <div>CategoryCard</div>
+        <div className='category_card'>CategoryCard</div>
     );
 };
 
