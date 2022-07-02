f71b2281851571467161797f034d8dec53f61b56
David Robison <drbr56@gmail.com>
2022-07-02 11:04:40 -0600

    Added two new divs to .category_card, with classNames of .category_title and .category_size Inside each new div, added a single p tag.

--- src/components/main/search/resultsCategories/CategoryCard.js
+++ src/components/main/search/resultsCategories/CategoryCard.js
@@ -3,7 +3,14 @@ import '../../../../styles/main/search/resultsCategories/CategoryCard.css';
 
 const CategoryCard = () => {
     return (
-        <div className='category_card'>CategoryCard</div>
+        <div className='category_card'>
+            <div className='category_title'>
+                <p></p>
+            </div>
+            <div className='category_size'>
+                <p></p>
+            </div>
+        </div>
     );
 };
 
