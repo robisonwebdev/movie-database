cbb29a6ee105154485620e3fc3a75a017ef21162
David Robison <drbr56@gmail.com>
2022-07-04 04:38:10 -0600

    Imported MediaCard and PersonCard components. Setup 3 varaibles to map over movieResults, peopleResults, or showsResults. Returning either MediaCard or PersonCard. Set showResults function that uses a switch statement that returns one of the three map variables. Called showResults in return section.

--- src/components/main/search/searchResults/SearchResults.js
+++ src/components/main/search/searchResults/SearchResults.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import MediaCard from './MediaCard';
+import PersonCard from './PersonCard';
 import '../../../../styles/main/search/searchResults/SearchResults.css';
 
-const SearchResults = () => {
+const SearchResults = ({ results, selectedResults }) => {
+    const  { movieResults, peopleResults, showsResults } = results;
+
+    const mapMovies = movieResults.results?.map(media => {
+        return <MediaCard key={media.id} media={media} />
+    });
+
+    const mapPeople = peopleResults.results?.map(person => {
+        return <PersonCard key={person.id} person={person} />
+    });
+
+    const mapTVShows = showsResults.results?.map(media => {
+        return <MediaCard key={media.id} media={media} />
+    });
+
+    const showResults = () => {
+        switch (selectedResults) {
+            case 'Movies':
+                return mapMovies;
+            case 'People':
+                return mapPeople;
+            case 'TV Shows':
+                return mapTVShows
+            default:
+                break;
+        }
+    };
+
     return (
-        <section className='search_results_container'>SearchResults</section>
+        <section className='search_results_container'>
+            {showResults()}
+        </section>
     );
 };
 
