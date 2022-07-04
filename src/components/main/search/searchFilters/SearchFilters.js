15860a708d4df36afcb7beacbcedd67dc8cf810b
David Robison <drbr56@gmail.com>
2022-07-04 04:02:53 -0600

    Destructured setSelectedResults. Setup handleOnClick function that calls setSelectedResults, getting passed selected argument. Passed handleOnClick down to each FilterCard component.

--- src/components/main/search/searchFilters/SearchFilters.js
+++ src/components/main/search/searchFilters/SearchFilters.js
@@ -2,17 +2,21 @@ import React from 'react';
 import FilterCard from './FilterCard';
 import '../../../../styles/main/search/searchFilters/SearchFilters.css';
 
-const SearchFilters = ({ filters }) => {
+const SearchFilters = ({ filters, setSelectedResults }) => {
     const { movieResults, peopleResults, showsResults } = filters;
+
+    const handleOnClick = (selected) => {
+        setSelectedResults(selected);
+    };
 
     return (
         <section className='search_filters'>
-            {console.log(results)}
+            {console.log(filters)}
             <div className='filters_header'>Search Results</div>
             <section className='filters'>
-                <FilterCard filter={movieResults} title='Movies' />
-                <FilterCard filter={peopleResults} title='People' />
-                <FilterCard filter={showsResults} title='TV Shows' />
+                <FilterCard filter={movieResults} handleOnClick={handleOnClick} title='Movies' />
+                <FilterCard filter={peopleResults} handleOnClick={handleOnClick} title='People' />
+                <FilterCard filter={showsResults} handleOnClick={handleOnClick} title='TV Shows' />
             </section>
         </section>
     );
