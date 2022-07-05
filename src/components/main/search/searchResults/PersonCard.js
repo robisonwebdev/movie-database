22096bd2a3dd8107925568437c88f520aa951468
David Robison <drbr56@gmail.com>
2022-07-05 15:59:49 -0600

    Imported PersonIcon. Setup getProfileImage function to get image from profile_path and return it in an img tag. If profile_path is null return PersonIcon. Called getProfileImage in .person_image div

--- src/components/main/search/searchResults/PersonCard.js
+++ src/components/main/search/searchResults/PersonCard.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import PersonIcon from '@mui/icons-material/Person';
 import '../../../../styles/main/search/searchResults/PersonCard.css';
 
 const PersonCard = ({ person }) => {
     const { known_for, known_for_department, name, profile_path } = person;
 
+    const getProfileImage = () => {
+        const profilePath = `https://image.tmdb.org/t/p/w90_and_h90_face/${profile_path}`;
+
+        if (profile_path === null) return <PersonIcon />
+
+        return <img src={profilePath} alt={name} />
+    };
+
     return (
         <section className='person_card'>
             {console.log(person)}
-            <div className='person_image'></div>
+            <div className='person_image'>
+                {getProfileImage()}
+            </div>
             <div className='person_info'>
                 <div className='person_name'></div>
                 <div className='person_known_for'></div>
