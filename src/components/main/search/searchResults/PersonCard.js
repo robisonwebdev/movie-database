3ee68224b8ea220f64d75fc1ca1b3cccbb268998
David Robison <drbr56@gmail.com>
2022-07-05 13:19:55 -0600

    Destructured known_for, known_for_department, name, and profile_path from person. Inside return section, add two new divs with classNames of .person_image and .person_info Inside person_info, added two new divs with classNames of .person_name and .person_known_for

--- src/components/main/search/searchResults/PersonCard.js
+++ src/components/main/search/searchResults/PersonCard.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import '../../../../styles/main/search/searchResults/PersonCard.css';
 
-const PersonCard = () => {
+const PersonCard = ({ person }) => {
+    const { known_for, known_for_department, name, profile_path } = person;
+
     return (
-        <section className='person_card'>PersonCard</section>
+        <section className='person_card'>
+            {console.log(person)}
+            <div className='person_image'></div>
+            <div className='person_info'>
+                <div className='person_name'></div>
+                <div className='person_known_for'></div>
+            </div>
+        </section>
     );
 };
 
