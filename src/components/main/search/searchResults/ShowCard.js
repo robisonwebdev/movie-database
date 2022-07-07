ea4962743f1c76efb2184c1a58ab5f175e86c675
David Robison <drbr56@gmail.com>
2022-07-07 10:04:23 -0600

    Imported MovieIcon Destructured first_air_date, name, overview, and poster_path from show. Setup getDate function, getOverview function, getName function, and getPoster function.

--- src/components/main/search/searchResults/ShowCard.js
+++ src/components/main/search/searchResults/ShowCard.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import MovieIcon from '@mui/icons-material/Movie';
 import '../../../../styles/main/search/searchResults/ShowCard.css'
 
 const ShowCard = ({ show }) => {
+    const { first_air_date, name, overview, poster_path } = show;
+
+    const getDate = () => {
+        const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        const getDate = new Date(first_air_date);
+        const year = getDate.getUTCFullYear();
+        const month = getDate.getUTCMonth();
+        const day = getDate.getUTCDate();
+
+        if (first_air_date === undefined) return null;
+
+        return `${monthNames[month]} ${day}, ${year}`;
+    };
+
+    const getOverview = () => {
+        if (overview === '' || overview === null) return null;
+
+        return overview;
+    };
+
+    const getName = () => {
+        if (name === '' || name === null) return null;
+
+        return name;
+    };
+
+    const getPoster = () => {
+        const posterPath = `https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${poster_path}`;
+
+        if (poster_path === null) return <MovieIcon />
+
+        return <img src={posterPath} alt={name} />
+    };
+    
     return (
         <section className='show_card'>ShowCard</section>
     );
