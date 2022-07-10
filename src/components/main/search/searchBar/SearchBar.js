0a442efc9a51cf76e38bdb1d9e93e599e2529259
David Robison <drbr56@gmail.com>
2022-07-10 11:01:18 -0600

    Imported useState. Destructured setSearchParams and value from prop. Setup searchValue state, and set to an initial value of value prop. Setup params object that sets query to searchValue. Setup handleInputChange to call setSearchValue, passing it event.target.value Setup handleSubmit function that preventsDefault and calls setSearchParams with params pass to it. Added form, input, and button to return section. Gave return section a className of .results_search_bar_container

--- src/components/main/search/searchBar/SearchBar.js
+++ src/components/main/search/searchBar/SearchBar.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../../../../styles/main/search/searchBar/SearchBar.css';
 
-const SearchBar = () => {
+const SearchBar = ({ setSearchParams, value }) => {
+    const [searchValue, setSearchValue] = useState(value);
+    let params = { query: searchValue };
+
+    const handleInputChange = (event) => {
+        setSearchValue(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setSearchParams(params);
+    };
+
     return (
-        <section className='search_bar'>SearchBar</section>
+        <section className='results_search_bar_container' onSubmit={handleSubmit}>
+            <form className='results_search_bar'>
+                <input type='text' value={searchValue} onChange={handleInputChange} />
+                <button>Search</button>
+            </form>
+        </section>
     );
 };
 
