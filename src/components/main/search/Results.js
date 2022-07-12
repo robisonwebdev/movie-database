0ab9d915689abf0e9a58a705ff568ab8a8f3c335
David Robison <drbr56@gmail.com>
2022-07-12 10:11:59 -0600

    Setup state for page, and gave an initial value of 1. Inside fetchData, replaced all API page number with page state. Added page state to dependencies of fetchData. Passed setPage down to SearchFilter. Passed page and setPage down to SearchResults.

--- src/components/main/search/Results.js
+++ src/components/main/search/Results.js
@@ -9,6 +9,7 @@ import '../../../styles/main/search/Results.css';
 const Results = () => {
     const [loading, setLoading] = useState(true);
     const [movieResults, setMovieResults] = useState([]);
+    const [page, setPage] = useState(1);
     const [peopleResults, setPeopleResults] = useState([]);
     const [selectedResults, setSelectedResults] = useState('Movies');
     const [showsResults, setShowsResults] = useState([]);
@@ -16,9 +17,9 @@ const Results = () => {
     const searchValue = searchParams.get('query');
 
     const fetchData = useCallback(() => {
-        const movie_API = `https://api.themoviedb.org/3/search/movie?api_key=9289aca3a6413b200619b263ac82e4c0&language=en-US&query=${searchValue}&page=1&include_adult=false`;
-        const people_API = `https://api.themoviedb.org/3/search/person?api_key=9289aca3a6413b200619b263ac82e4c0&language=en-US&query=${searchValue}&page=1&include_adult=false`;
-        const shows_API = `https://api.themoviedb.org/3/search/tv?api_key=9289aca3a6413b200619b263ac82e4c0&language=en-US&page=1&query=${searchValue}&include_adult=false`;
+        const movie_API = `https://api.themoviedb.org/3/search/movie?api_key=9289aca3a6413b200619b263ac82e4c0&language=en-US&query=${searchValue}&page=${page}&include_adult=false`;
+        const people_API = `https://api.themoviedb.org/3/search/person?api_key=9289aca3a6413b200619b263ac82e4c0&language=en-US&query=${searchValue}&page=${page}&include_adult=false`;
+        const shows_API = `https://api.themoviedb.org/3/search/tv?api_key=9289aca3a6413b200619b263ac82e4c0&language=en-US&page=${page}&query=${searchValue}&include_adult=false`;
 
         const getMovie_Data = axios.get(movie_API);
         const getPeople_Data = axios.get(people_API);
@@ -39,7 +40,7 @@ const Results = () => {
             setLoading(false);
         }))
         .catch(err => console.log(err))
-    }, [searchValue]);
+    }, [page, searchValue]);
     
     useEffect(() => {
         fetchData();
@@ -51,8 +52,8 @@ const Results = () => {
                 <SearchBar setSearchParams={setSearchParams} value={searchValue} />
             </div>
             <div className='results_area_2'>
-                <SearchFilters filters={{movieResults: movieResults, peopleResults: peopleResults, showsResults: showsResults}} setSelectedResults={setSelectedResults} />
-                <SearchResults results={{movieResults: movieResults, peopleResults: peopleResults, showsResults: showsResults}} selectedResults={selectedResults} />
+                <SearchFilters setPage={setPage} filters={{movieResults: movieResults, peopleResults: peopleResults, showsResults: showsResults}} setSelectedResults={setSelectedResults} />
+                <SearchResults page={page} setPage={setPage} results={{movieResults: movieResults, peopleResults: peopleResults, showsResults: showsResults}} selectedResults={selectedResults} />
             </div>            
         </section>
     );
