cd83003e853e72968ef2c7ea77730f295b657be8
David Robison <drbr56@gmail.com>
2022-07-14 09:43:25 -0600

    Imported useCallback, useEffect, useState, axios, and breakpoint. Setup state for loading, showData, and width.

--- src/components/main/show/Show.js
+++ src/components/main/show/Show.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import breakpoint from '../../config/breakpoint';
 import '../../../styles/main/show/Show.css';
 
 const Show = () => {
     const { showID } = useParams();
+    const [loading, setLoading ] = useState(true);
+    const [showData, setShowData] = useState([]);
+    const [width, setWidth] = useState(window.innerWidth);
 
     return (
         <section className='show'>Show</section>
