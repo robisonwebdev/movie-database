2c2d0a7560ed0bb9de5ccd5abb2eef992c827a08
David Robison <drbr56@gmail.com>
2022-07-16 11:10:58 -0600

    Imported ShowDesktop and ShowMobile components. Added code to useEffect to handle window resizing that will determine when to show ShowMobile or ShowDesktop components.

--- src/components/main/show/Show.js
+++ src/components/main/show/Show.js
@@ -2,6 +2,8 @@ import React, { useCallback, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import breakpoint from '../../config/breakpoint';
+import ShowDesktop from './ShowDesktop';
+import ShowMobile from './ShowMobile';
 import '../../../styles/main/show/Show.css';
 
 const Show = () => {
@@ -16,10 +18,23 @@ const Show = () => {
 
     useEffect(() => {
         fetchData();
+
+        const handleWindowResize = () => {
+            setWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => window.removeEventListener('resize', handleWindowResize);
     }, [fetchData]);
 
     return (
-        <section className='show'>Show</section>
+        <section className='show'>
+            {loading 
+                ? null
+                : width < breakpoint ? <ShowMobile /> : <ShowDesktop />
+            }
+        </section>
     );
 };
 
