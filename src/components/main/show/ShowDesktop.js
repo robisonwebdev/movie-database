caf39faf51855da6bfe02791454b2a5dfdcb92dc
David Robison <drbr56@gmail.com>
2022-07-17 10:37:50 -0600

    Imported DesktopHeader component. Added DesktopHeader to return section. Added new seciton with a className of .show_desktop_header to return section.

--- src/components/main/show/ShowDesktop.js
+++ src/components/main/show/ShowDesktop.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import DesktopHeader from './header/DesktopHeader';
 import '../../../styles/main/show/ShowDesktop.css';
 
 const ShowDesktop = () => {
     return (
-        <section className='show_desktop'>ShowDesktop</section>
+        <section className='show_desktop'>
+            <DesktopHeader />
+            <section className='show_desktop_main'>Main</section>
+        </section>
     );
 };
 
