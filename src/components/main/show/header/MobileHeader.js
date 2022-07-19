80169deda685ba9af020dceb42aaf491925aaacd
David Robison <drbr56@gmail.com>
2022-07-19 11:36:59 -0600

    Imported headerBackground. Destructured show prop. Added new div with className of .show_mobile_background, and passed it headerBackground in style event.

--- src/components/main/show/header/MobileHeader.js
+++ src/components/main/show/header/MobileHeader.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import headerBackground from './backgroundStyling';
 import '../../../../styles/main/show/header/MobileHeader.css';
 
-const MobileHeader = () => {
+const MobileHeader = ({ show }) => {
     return (
-        <section className='show_mobile_header'>MobileHeader</section>
+        <section className='show_mobile_header'>
+            <div className='show_mobile_background' style={headerBackground(show)}></div>
+            {console.log(show)}
+        </section>
     );
 };
 
