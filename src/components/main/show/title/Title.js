88e3b0fb188f4554352c67b5e4fc6a11108eb90d
David Robison <drbr56@gmail.com>
2022-07-19 11:48:54 -0600

    Setup getAirFirstDate function to return show year. Added new p tag to return seciton and called getAirFirstDate inside tag.

--- src/components/main/show/title/Title.js
+++ src/components/main/show/title/Title.js
@@ -4,9 +4,19 @@ import '../../../../styles/main/show/title/Title.css';
 const Title = ({ show }) => {
     const { first_air_date, name } = show;
 
+    const getAirFirstDate = () => {
+        const getDate = new Date(first_air_date);
+        const getYear = getDate.getUTCFullYear(getDate);
+
+        if (getDate === null) return null;
+
+        return `(${getYear})`;
+    };
+
     return (
         <section className='show_title'>
             <h3>{name}</h3>
+            <p>{getAirFirstDate()}</p>
         </section>
     );
 };
