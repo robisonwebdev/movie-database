823d64583093900d180a22d8fea7661f94220b30
David Robison <drbr56@gmail.com>
2022-07-19 11:44:56 -0600

    Imported Title component. Added Title component below .show_mobile_background div, and passed it show prop.

--- src/components/main/show/header/MobileHeader.js
+++ src/components/main/show/header/MobileHeader.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import headerBackground from './backgroundStyling';
+import Title from '../title/Title';
 import '../../../../styles/main/show/header/MobileHeader.css';
 
 const MobileHeader = ({ show }) => {
     return (
         <section className='show_mobile_header'>
             <div className='show_mobile_background' style={headerBackground(show)}></div>
+            <Title show={show} />
             {console.log(show)}
         </section>
     );
