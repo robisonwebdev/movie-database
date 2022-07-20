958d9a642509803087ae0e525c7ac2d44891102f
David Robison <drbr56@gmail.com>
2022-07-20 12:39:24 -0600

    Imported Genres, Rating, Runtime, and ShowDate components. Destructured show prop.

--- src/components/main/show/facts/Facts.js
+++ src/components/main/show/facts/Facts.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Genres from './Genres';
+import Rating from './Rating';
+import Runtime from './Runtime';
+import ShowDate from './ShowDate';
 import '../../../../styles/main/show/facts/Facts.css';
 
-const Facts = () => {
+const Facts = ({ show }) => {
     return (
         <section className='show_facts'>Facts</section>
     );
