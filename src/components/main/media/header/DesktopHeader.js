a226191d25a34f98c7600fc83701d5c79e66e8ed
David Robison <drbr56@gmail.com>
2022-07-21 11:35:58 -0600

    Imported DesktopHeader.css Gave return section a className of .media_desktop_header

--- src/components/main/media/header/DesktopHeader.js
+++ src/components/main/media/header/DesktopHeader.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../../../../styles/main/media/header/DesktopHeader.css';
 
 const DesktopHeader = () => {
     return (
-        <section>DesktopHeader</section>
+        <section className='media_desktop_header'>DesktopHeader</section>
     );
 };
 
