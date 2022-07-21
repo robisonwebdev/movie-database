013057f7a5e054b9db9f16905a3640e167f6244a
David Robison <drbr56@gmail.com>
2022-07-21 12:26:48 -0600

    Imported Title component. Added Title component to return section.

--- src/components/main/media/header/MobileHeader.js
+++ src/components/main/media/header/MobileHeader.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import backdropStyling from '../../../config/backdropStyling';
+import Title from './Title';
 import '../../../../styles/main/media/header/MobileHeader.css';
 
-const MobileHeader = () => {
+const MobileHeader = ({ media }) => {
     return (
         <section className='media_mobile_header'>
             <div className='media_backdrop' style={backdropStyling()}></div>
+            <Title />
         </section>
     );
 };
