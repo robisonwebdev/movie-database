852df800d325f2ca2a7dc60407ca86a7e187425f
David Robison <drbr56@gmail.com>
2022-07-21 12:40:58 -0600

    Replaced DesktopHeader component with new DesktopHeader component located in media folder. Removed isMobile prop. Added new section with className of .movie_desktop_main

--- src/components/main/movie/MovieDesktop.js
+++ src/components/main/movie/MovieDesktop.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import DesktopHeader from './header/DesktopHeader';
+import DesktopHeader from '../media/header/DesktopHeader';
 import '../../../styles/main/movie/MovieDesktop.css';
 
-const MovieDesktop = ({ isMobile, movie }) => {
+const MovieDesktop = ({ movie }) => {
     
     return (
         <section className='movie_desktop'>
-           <DesktopHeader isMobile={isMobile} movie={movie} />
+           <DesktopHeader media={movie} />
+           <section className='movie_desktop_main'>Main</section>
         </section>
     );
 };
