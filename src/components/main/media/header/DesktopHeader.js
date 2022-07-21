3c1d3b9858f83bbd9b5c40f61f5bb426127600ed
David Robison <drbr56@gmail.com>
2022-07-21 12:46:59 -0600

    Added new div to return section. Inside div added two new sections with classNames of .media_header_left and .media_header_right

--- src/components/main/media/header/DesktopHeader.js
+++ src/components/main/media/header/DesktopHeader.js
@@ -4,7 +4,12 @@ import '../../../../styles/main/media/header/DesktopHeader.css';
 
 const DesktopHeader = ({ media }) => {
     return (
-        <section className='media_desktop_header' style={backdropStyling()}>DesktopHeader</section>
+        <section className='media_desktop_header' style={backdropStyling()}>
+            <div>
+                <section className='media_header_left'></section>
+                <section className='media_header_right'></section>
+            </div>
+        </section>
     );
 };
 
