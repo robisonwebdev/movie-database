da898e37100539a59a5a94cae47e32af340ae783
David Robison <drbr56@gmail.com>
2022-07-21 12:37:08 -0600

    Replaced MobileHeader with new MobileHeader component.

--- src/components/main/show/ShowMobile.js
+++ src/components/main/show/ShowMobile.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import MobileHeader from './header/MobileHeader';
+import MobileHeader from '../media/header/MobileHeader';
 import '../../../styles/main/show/ShowMobile.css';
 
 const ShowMobile = ({ show }) => {
     return (
         <section className='show_mobile'>
-            <MobileHeader show={show} />
+            <MobileHeader media={show} />
             <section className='show_mobile_main'>Main</section>
         </section>
     );
