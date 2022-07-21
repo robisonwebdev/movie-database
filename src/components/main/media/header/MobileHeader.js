7ffa36689c4378cdf329f7c6922e46871ead2434
David Robison <drbr56@gmail.com>
2022-07-21 12:33:40 -0600

    Imported Poster component. Added Poster component to .media_backdrop div

--- src/components/main/media/header/MobileHeader.js
+++ src/components/main/media/header/MobileHeader.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Facts from './Facts';
 import Overview from './Overview';
+import Poster from './Poster';
 import Tagline from './Tagline';
 import Title from './Title';
 import backdropStyling from '../../../config/backdropStyling';
@@ -9,7 +10,9 @@ import '../../../../styles/main/media/header/MobileHeader.css';
 const MobileHeader = ({ media }) => {
     return (
         <section className='media_mobile_header'>
-            <div className='media_backdrop' style={backdropStyling()}></div>
+            <div className='media_backdrop' style={backdropStyling()}>
+                <Poster />
+            </div>
             <Title />
             <Facts />
             <Tagline />
