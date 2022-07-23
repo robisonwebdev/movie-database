42f1a61cdaf0139bb968c6933b9f46ee501d582f
David Robison <drbr56@gmail.com>
2022-07-23 00:04:42 -0600

    Imported Genres, MediaDate, Rating, and Runtime components.

--- src/components/main/media/header/Facts.js
+++ src/components/main/media/header/Facts.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Genres from './Genres';
+import MediaDate from './MediaDate';
+import Rating from './Rating';
+import Runtime from './Runtime';
 
 const Facts = () => {
     return (
