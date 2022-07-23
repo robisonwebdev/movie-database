cd377d54ff85abc1309242d753eef14e743148ac
David Robison <drbr56@gmail.com>
2022-07-23 00:06:35 -0600

    Imported Facts.css Destructured media prop.

--- src/components/main/media/header/Facts.js
+++ src/components/main/media/header/Facts.js
@@ -3,8 +3,9 @@ import Genres from './Genres';
 import MediaDate from './MediaDate';
 import Rating from './Rating';
 import Runtime from './Runtime';
+import '../../../../styles/main/media/header/Facts.css';
 
-const Facts = () => {
+const Facts = ({ media }) => {
     return (
         <section>Facts</section>
     );
