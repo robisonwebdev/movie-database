b5997228a6df058843bbaed4ce9395bbaa0cd3fd
David Robison <drbr56@gmail.com>
2022-07-25 10:03:19 -0600

    Setup getTagline function to return either null or tagline, depending on if tagline exist. Return section from component return and replaced with empty tag. Called getTagline inside of empty tag.

--- src/components/main/media/header/Tagline.js
+++ src/components/main/media/header/Tagline.js
@@ -4,8 +4,16 @@ import '../../../../styles/main/media/header/Tagline.css';
 const Tagline = ({ media }) => {
     const { tagline } = media;
 
+    const getTagline = () => {
+        if (tagline === '' || tagline === null) return null;
+
+        return <h4 className='media_tagline'>{tagline}</h4>;
+    };
+
     return (
-        <section className='media_tagline'>Tagline</section>
+        <>
+            {getTagline()}
+        </>
     );
 };
 
