abfdba716a9bab36fd57f576daea6e75eeccffcc
David Robison <drbr56@gmail.com>
2022-07-26 10:05:26 -0600

    Destructured media prop from component. Destructured name and title from media.

--- src/components/main/media/header/Title.js
+++ src/components/main/media/header/Title.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import '../../../../styles/main/media/header/Title.css';
 
-const Title = () => {
+const Title = ({ media }) => {
+    const { name, title } = media;
+
     return (
         <section className='media_title'>Title</section>
     );
