7cd4cf387a4f3fcc3e656694c9bb098d4d6c61d2
David Robison <drbr56@gmail.com>
2022-07-27 09:43:21 -0600

    Imported useCallback. Setup rating state, and set to an initial value of 'N/A'. Fixed typo in content_ratings. Setup getContentRating fucntion to setRating using content_rating. Setup getReleaseRating to setRating using release_dates. Setup useEffect to call either getReleaseRating or getContentRating. Changed return section to div. Added child p tag to return div, and called rating state inside it.

--- src/components/main/media/header/Rating.js
+++ src/components/main/media/header/Rating.js
@@ -1,11 +1,42 @@
-import React, {useEffect, useState} from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import '../../../../styles/main/media/header/Rating.css';
 
 const Rating = ({ media }) => {
-    const { content_rating, release_dates } = media;
+    const { content_ratings, release_dates } = media;
+    const [rating, setRating] = useState('N/A');
+
+    const getContentRating = useCallback(() => {
+        const USRating = content_ratings.results.filter(obj => obj['iso_3166_1'] === 'US');
+
+        if (USRating.lenght === 0) return null;
+
+        setRating(USRating[0]['rating']);
+    }, [content_ratings]);
+
+    const getReleaseRating = useCallback(() => {
+        const USReleaseDates = release_dates.results.filter(obj => obj['iso_3166_1'] === 'US');
+
+        if (USReleaseDates.length === 0) return null;
+
+        setRating(USReleaseDates[0]['release_dates'][0]['certification']);
+    }, [release_dates]);
+
+    useEffect(() => {
+        if (content_ratings === undefined) {
+            getReleaseRating();
+        }
+
+        if (release_dates === undefined) {
+            getContentRating();
+        }
+    }, [content_ratings, getContentRating, getReleaseRating, release_dates]);
 
     return (
-        <section className='media_rating'>Rating</section>
+        <div className='media_rating'>
+            <p>
+                {rating}
+            </p>
+        </div>
     );
 };
 
