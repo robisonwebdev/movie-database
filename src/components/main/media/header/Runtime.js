824bf0b388e269a1a797b906c784f3171425c88f
David Robison <drbr56@gmail.com>
2022-07-27 08:59:42 -0600

    Imported useEffect and useState. Setup value state, with an initial value of NaN. Setup getRuntime function to convert state value into hours and/or minutes. If value is NaN return 0m, else return runtime. Setup useEffect to setValue with either episode_run_time or runtime. Called getRuntime in return div.

--- src/components/main/media/header/Runtime.js
+++ src/components/main/media/header/Runtime.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../../../styles/main/media/header/Runtime.css';
 
 const Runtime = ({ media }) => {
     const { episode_run_time, runtime} = media;
+    const [value, setValue] = useState(NaN);
+    
+    const getRuntime = () => {
+        const runtimeToHours = (value / 60);
+        const hours = Math.floor(runtimeToHours);
+        const minutes = Math.floor((runtimeToHours - hours) * 60);
+
+        if (isNaN(value)) return '0m';
+
+        return hours >=1 ? `${hours}h ${minutes}m` : `${minutes}m`;
+    };
+
+    useEffect(() => {
+        if(episode_run_time === undefined) {
+            setValue(runtime);
+        }
+
+        if (runtime === undefined) {
+            setValue(episode_run_time[0])
+        }
+    }, [episode_run_time, runtime]);
 
     return (
-        <section className='media_runtime'>Runtime</section>
+        <div className='media_runtime'>
+            {getRuntime()}
+        </div>
     );
 };
 
