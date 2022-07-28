656be92d4f81edd7833bdf0d6c14fa154ec53e08
David Robison <drbr56@gmail.com>
2022-07-28 09:11:29 -0600

    Imported MovieIcon Destructured name and title from media. Setup getPoster to return MovieIcon if movie poster is null, or and image set to poster url. Changed return section to a div. Called getPoster in return div.

--- src/components/main/media/header/Poster.js
+++ src/components/main/media/header/Poster.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import MovieIcon from '@mui/icons-material/Movie';
 import '../../../../styles/main/media/header/Poster.css';
 
 const Poster = ({ media }) => {
-    const { poster_path } = media;
+    const { name, poster_path, title } = media;
+
+    const getPoster = () => {
+        const posterURL = `https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${poster_path}`;
+
+        if (poster_path === null) return <MovieIcon />
+
+        return <img src={posterURL} alt={name || title} />
+    };
 
     return (
-        <section className='media_poster'>
-            {console.log(media)}
-        </section>
+        <div className='media_poster'>
+            {getPoster()}
+        </div>
     );
 };
 
