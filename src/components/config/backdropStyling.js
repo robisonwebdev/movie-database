01f39804d58f449b930a1af750224ee5536b062e
David Robison <drbr56@gmail.com>
2022-07-31 10:31:48 -0600

    Changed background linear-gradient settings.

--- src/components/config/backdropStyling.js
+++ src/components/config/backdropStyling.js
@@ -2,7 +2,7 @@ const headerBackground = (media) => {
     const { backdrop_path } = media;
     const backgroundURL = `http://image.tmdb.org/t/p/original/${backdrop_path}`;
     const styling = {
-        background: `linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.9)), url(${backgroundURL})`,
+        background: `linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.9)), url(${backgroundURL})`,
         backgroundPosition: 'center top',
         backgroundRepeat: 'no-repeat',
         backgroundSize: 'cover'
