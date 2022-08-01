fccb46e4f4b50a99e06e8596449377d747ef24c3
David Robison <drbr56@gmail.com>
2022-08-01 03:51:06 -0600

    Imported Scroller component. Added two new sections to .movie_desktop_main, with classNames of .desktop_main_left and .desktop_main_right Called Scroller component in .desktop_main_left Passed movie down to Scroller component.

--- src/components/main/movie/MovieDesktop.js
+++ src/components/main/movie/MovieDesktop.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import DesktopHeader from '../media/header/DesktopHeader';
+import Scroller from '../media/scroller/Scroller';
 import '../../../styles/main/movie/MovieDesktop.css';
 
 const MovieDesktop = ({ movie }) => {
@@ -7,7 +8,12 @@ const MovieDesktop = ({ movie }) => {
     return (
         <section className='movie_desktop'>
            <DesktopHeader media={movie} />
-           <section className='movie_desktop_main'>Main</section>
+           <section className='movie_desktop_main'>
+                <section className='desktop_main_left'>
+                    <Scroller media={movie} />
+                </section>
+                <section className='desktop_main_right'></section>
+           </section>
         </section>
     );
 };
