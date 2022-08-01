5e5905561801aed6eed9fe6473deeb7e0c685fac
David Robison <drbr56@gmail.com>
2022-08-01 04:20:27 -0600

    Destructured title from component. Added h2 with title prop to .media_scroller Added new div with className of .scroller_cards to .media_scroller, and moved buildScrollerCard into it.

--- src/components/main/media/scroller/Scroller.js
+++ src/components/main/media/scroller/Scroller.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ScrollerCard from './ScrollerCard';
 import '../../../../styles/main/media/scroller/Scroller.css';
 
-const Scroller = ({ content, show }) => {
+const Scroller = ({ content, show, title }) => {
     const slicedContent = content.slice(0, (show || 10));
 
     const buildScrollerCards = slicedContent?.map(item => {
@@ -12,7 +12,10 @@ const Scroller = ({ content, show }) => {
     return (
         <section className='media_scroller'>
             {console.log(content)}
-            {buildScrollerCards}
+            <h2>{title}</h2>
+            <div className='scroller_cards'>
+                {buildScrollerCards}
+            </div>
         </section>
     );
 };
