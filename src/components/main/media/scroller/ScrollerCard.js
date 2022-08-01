41bc17bbd9d1c4a6f209829d835f01795efc5d62
David Robison <drbr56@gmail.com>
2022-08-01 04:05:43 -0600

    Imported ScrollerCard.css Gave return section a className of .media_scroller_card

--- src/components/main/media/scroller/ScrollerCard.js
+++ src/components/main/media/scroller/ScrollerCard.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../../../../styles/main/media/scroller/ScrollerCard.css';
 
 const ScrollerCard = () => {
     return (
-        <section>ScrollerCard</section>
+        <section className='media_scroller_card'>ScrollerCard</section>
     );
 };
 
