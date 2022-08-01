410a6b87b8561650c33f2b8fc9456d6118ffce4d
David Robison <drbr56@gmail.com>
2022-08-01 04:17:10 -0600

    Imported ScrollerCard component. Destructured show from component. Setup slicedContent to slice down content to show or 10. Setup buildScrollerCards that take slicedContent and map over it, return ScrollerCard. Called buildScrollerCards in .media_scroller

--- src/components/main/media/scroller/Scroller.js
+++ src/components/main/media/scroller/Scroller.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import ScrollerCard from './ScrollerCard';
 import '../../../../styles/main/media/scroller/Scroller.css';
 
-const Scroller = ({ content }) => {
+const Scroller = ({ content, show }) => {
+    const slicedContent = content.slice(0, (show || 10));
+
+    const buildScrollerCards = slicedContent?.map(item => {
+        return <ScrollerCard key={item.id} cardInfo={item} />
+    });
+
     return (
         <section className='media_scroller'>
             {console.log(content)}
+            {buildScrollerCards}
         </section>
     );
 };
