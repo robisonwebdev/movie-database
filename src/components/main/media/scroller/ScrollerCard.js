4dc4f77656492fc0114a21ea7197cbfbf74b373e
David Robison <drbr56@gmail.com>
2022-08-02 10:55:34 -0600

    Destructured character, name, and profile_path from cardInfo. Setup blank getCardImage function.

--- src/components/main/media/scroller/ScrollerCard.js
+++ src/components/main/media/scroller/ScrollerCard.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import '../../../../styles/main/media/scroller/ScrollerCard.css';
 
-const ScrollerCard = () => {
+const ScrollerCard = ({ cardInfo }) => {
+    const { character, name, profile_path } = cardInfo;
+
+    const getCardImage = () => {};
+
     return (
         <section className='media_scroller_card'>ScrollerCard</section>
     );
