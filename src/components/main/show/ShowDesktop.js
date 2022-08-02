0a54477bb2ef9b9cba65492702fe933f82932d4a
David Robison <drbr56@gmail.com>
2022-08-02 11:00:32 -0600

    Imported Scroller component. Called Scroller component in .show_desktop_left. Passed Scroller component show.credits.cast

--- src/components/main/show/ShowDesktop.js
+++ src/components/main/show/ShowDesktop.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import DesktopHeader from '../media/header/DesktopHeader';
+import Scroller from '../media/scroller/Scroller';
 import '../../../styles/main/show/ShowDesktop.css';
 
 const ShowDesktop = ({ show }) => {
@@ -7,7 +8,9 @@ const ShowDesktop = ({ show }) => {
         <section className='show_desktop'>
             <DesktopHeader media={show} />
             <section className='show_desktop_main'>
-                <section className='show_desktop_left'></section>
+                <section className='show_desktop_left'>
+                    <Scroller content={show.credits.cast} slice={10} title='Series Cast' />
+                </section>
                 <section className='show_desktop_right'></section>
             </section>
         </section>
