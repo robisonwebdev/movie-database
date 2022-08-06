b350d94caed22adfc1ebe590640e695fac8902cf
David Robison <drbr56@gmail.com>
2022-08-06 10:12:30 -0600

    Setup logoAddress const that plugs logo_path into url to get logo image. Added img tag to .network_card with src to logoAddress and alt to name.

--- src/components/main/media/networks/NetworkCard.js
+++ src/components/main/media/networks/NetworkCard.js
@@ -4,8 +4,12 @@ import '../../../../styles/main/media/networks/NetworkCard.css';
 const NetworkCard = ({ network }) => {
     const { logo_path, name } = network;
 
+    const logoAddress  = `https://www.themoviedb.org/t/p/h30/${logo_path}`;
+
     return (
-        <div className='network_card'>NetworkCard</div>
+        <div className='network_card'>
+            <img src={logoAddress} alt={name} />
+        </div>
     );
 };
 
