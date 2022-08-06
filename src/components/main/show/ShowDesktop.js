966a3cd0047b881237e2375b5ca5072c8c287034
David Robison <drbr56@gmail.com>
2022-08-06 10:07:43 -0600

    Imported Networks component. Called Networks component in .show_desktop_right. Passed Networks component show prop.

--- src/components/main/show/ShowDesktop.js
+++ src/components/main/show/ShowDesktop.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import DesktopHeader from '../media/header/DesktopHeader';
+import Networks from '../media/networks/Networks';
 import Scroller from '../media/scroller/Scroller';
 import '../../../styles/main/show/ShowDesktop.css';
 
@@ -11,7 +12,9 @@ const ShowDesktop = ({ show }) => {
                 <section className='show_desktop_left'>
                     <Scroller content={show.credits.cast} slice={10} title='Series Cast' />
                 </section>
-                <section className='show_desktop_right'></section>
+                <section className='show_desktop_right'>
+                    <Networks media={show} />
+                </section>
             </section>
         </section>
     );
