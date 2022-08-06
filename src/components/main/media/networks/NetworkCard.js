8f881a0928bc39614713d7645ccaa3c053a45a48
David Robison <drbr56@gmail.com>
2022-08-06 09:58:30 -0600

    Imported NetworkCard.css Gave return section a className of .network_card

--- src/components/main/media/networks/NetworkCard.js
+++ src/components/main/media/networks/NetworkCard.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../../../../styles/main/media/networks/NetworkCard.css';
 
 const NetworkCard = () => {
     return (
-        <div>NetworkCard</div>
+        <div className='network_card'>NetworkCard</div>
     );
 };
 
