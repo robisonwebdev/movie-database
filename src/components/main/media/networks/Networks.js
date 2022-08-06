419c6fbc653996ff1decbd7b0da04e6366fc3a4e
David Robison <drbr56@gmail.com>
2022-08-06 10:01:40 -0600

    Imported NewworkCard component. Setup buildNetworksList const to map over networks, returning NetworkCard for each. Called buildNetworksList in .networks_list div.

--- src/components/main/media/networks/Networks.js
+++ src/components/main/media/networks/Networks.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import NetworkCard from './NetworkCard';
 import '../../../../styles/main/media/networks/Networks.css';
 
 const Networks = ({ media }) => {
     const { networks } = media;
 
+    const buildNetworksList = networks?.map(network => {
+        return <NetworkCard key={network.id} network={network} />
+    });
+
     return (
         <section className='media_networks'>
             <h3>Networks</h3>
             <div className='networks_list'>
-                
+                {buildNetworksList}
             </div>
         </section>
     );
